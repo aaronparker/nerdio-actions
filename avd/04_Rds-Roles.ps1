48fc4d0a20378d4f63aacc8f860746bf9b7c64d7
Aaron Parker <aaron@stealthpuppy.com>
2022-11-08 13:17:57 +1100

    Change error output

--- avd/04_Rds-Roles.ps1
+++ avd/04_Rds-Roles.ps1
@@ -11,52 +11,40 @@ switch -Regex ((Get-CimInstance -ClassName "CIM_OperatingSystem").Caption) {
                 FeatureName   = "Printing-XPSServices-Features"
                 Online        = $true
                 NoRestart     = $true
-                WarningAction = "Continue"
-                ErrorAction   = "Continue"
+                WarningAction = "SilentlyContinue"
+                ErrorAction   = "SilentlyContinue"
             }
             Disable-WindowsOptionalFeature @params
         }
         catch {
-            throw $_.Exception.Message
+            $_.Exception.Message
         }
 
         try {
             $params = @{
                 Name                   = "EnhancedStorage", "PowerShell-ISE"
                 IncludeManagementTools = $true
-                WarningAction          = "Continue"
-                ErrorAction            = "Continue"
+                WarningAction          = "SilentlyContinue"
+                ErrorAction            = "SilentlyContinue"
             }
             Uninstall-WindowsFeature @params
+        }
+        catch {
+            $_.Exception.Message
+        }
+
+        try {
+            $params = @{
+                Name          = "RDS-RD-Server", "Server-Media-Foundation", "Search-Service", "NET-Framework-Core", "Remote-Assistance"
+                WarningAction = "SilentlyContinue"
+                ErrorAction   = "SilentlyContinue"
+            }
+            Install-WindowsFeature @params
         }
         catch {
             throw $_.Exception.Message
         }
 
-        $params = @{
-            Name          = "RDS-RD-Server", "Server-Media-Foundation", "Search-Service", "NET-Framework-Core", "Remote-Assistance"
-            WarningAction = "Continue"
-            ErrorAction   = "Continue"
-        }
-        Install-WindowsFeature @params
-
-        # Enable services
-        if ((Get-WindowsFeature -Name "RDS-RD-Server").InstallState -eq "Installed") {
-            foreach ($service in "Audiosrv", "WSearch") {
-                try {
-                    $params = @{
-                        Name          = $service
-                        StartupType   = "Automatic"
-                        WarningAction = "Continue"
-                        ErrorAction   = "Continue"
-                    }
-                    Set-Service @params
-                }
-                catch {
-                    throw $_.Exception.Message
-                }
-            }
-        }
         break
     }
     "Microsoft Windows 1* Enterprise for Virtual Desktops|Microsoft Windows 1* Enterprise|Microsoft Windows 1*" {
@@ -67,8 +55,8 @@ switch -Regex ((Get-CimInstance -ClassName "CIM_OperatingSystem").Caption) {
                     "MicrosoftWindowsPowerShellV2"
                 Online        = $true
                 NoRestart     = $true
-                WarningAction = "Continue"
-                ErrorAction   = "Continue"
+                WarningAction = "SilentlyContinue"
+                ErrorAction   = "SilentlyContinue"
             }
             Disable-WindowsOptionalFeature @params
         }
@@ -82,8 +70,8 @@ switch -Regex ((Get-CimInstance -ClassName "CIM_OperatingSystem").Caption) {
                     "App.Support.QuickAssist~~~~0.0.1.0", "MathRecognizer~~~~0.0.1.0", `
                     "Browser.InternetExplorer~~~~0.0.11.0", "Print.Fax.Scan~~~~0.0.1.0"
                 IncludeManagementTools = $true
-                WarningAction          = "Continue"
-                ErrorAction            = "Continue"
+                WarningAction          = "SilentlyContinue"
+                ErrorAction            = "SilentlyContinue"
             }
             Uninstall-WindowsFeature @params
         }
@@ -104,8 +92,8 @@ if ((Get-WindowsFeature -Name "RDS-RD-Server").InstallState -eq "Installed") {
             $params = @{
                 Name          = $service
                 StartupType   = "Automatic"
-                WarningAction = "Continue"
-                ErrorAction   = "Continue"
+                WarningAction = "SilentlyContinue"
+                ErrorAction   = "SilentlyContinue"
             }
             Set-Service @params
         }
