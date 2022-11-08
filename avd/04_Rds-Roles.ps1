89acf966c98d4c00f6b7e48e5a048d98e25225e3
Aaron Parker <aaron@stealthpuppy.com>
2022-11-08 13:05:55 +1100

    Update 04_Rds-Roles.ps1
    Address issue with script running on Windows Server

--- avd/04_Rds-Roles.ps1
+++ avd/04_Rds-Roles.ps1
@@ -4,35 +4,97 @@
 
 #region Script logic
 # Add / Remove roles and features (requires reboot at end of deployment)
-try {
-    $params = @{
-        FeatureName   = "Printing-XPSServices-Features", "SMB1Protocol", "WorkFolders-Client", `
-            "FaxServicesClientPackage", "WindowsMediaPlayer", "MicrosoftWindowsPowerShellV2Root", `
-            "MicrosoftWindowsPowerShellV2"
-        Online        = $true
-        NoRestart     = $true
-        WarningAction = "Continue"
-        ErrorAction   = "Continue"
-    }
-    Disable-WindowsOptionalFeature @params
-}
-catch {
-    throw $_.Exception.Message
-}
+switch -Regex ((Get-CimInstance -ClassName "CIM_OperatingSystem").Caption) {
+    "Microsoft Windows Server*" {
+        try {
+            $params = @{
+                FeatureName   = "Printing-XPSServices-Features"
+                Online        = $true
+                NoRestart     = $true
+                WarningAction = "Continue"
+                ErrorAction   = "Continue"
+            }
+            Disable-WindowsOptionalFeature @params
+        }
+        catch {
+            throw $_.Exception.Message
+        }
 
-try {
-    $params = @{
-        Name                   = "Media.WindowsMediaPlayer~~~~0.0.12.0", "XPS.Viewer~~~~0.0.1.0", `
-            "App.Support.QuickAssist~~~~0.0.1.0", "MathRecognizer~~~~0.0.1.0", `
-            "Browser.InternetExplorer~~~~0.0.11.0", "Print.Fax.Scan~~~~0.0.1.0"
-        IncludeManagementTools = $true
-        WarningAction          = "Continue"
-        ErrorAction            = "Continue"
+        try {
+            $params = @{
+                Name                   = "EnhancedStorage", "PowerShell-ISE"
+                IncludeManagementTools = $true
+                WarningAction          = "Continue"
+                ErrorAction            = "Continue"
+            }
+            Uninstall-WindowsFeature @params
+        }
+        catch {
+            throw $_.Exception.Message
+        }
+
+        $params = @{
+            Name          = "RDS-RD-Server", "Server-Media-Foundation", "Search-Service", "NET-Framework-Core", "Remote-Assistance"
+            WarningAction = "Continue"
+            ErrorAction   = "Continue"
+        }
+        Install-WindowsFeature @params
+
+        # Enable services
+        if ((Get-WindowsFeature -Name "RDS-RD-Server").InstallState -eq "Installed") {
+            foreach ($service in "Audiosrv", "WSearch") {
+                try {
+                    $params = @{
+                        Name          = $service
+                        StartupType   = "Automatic"
+                        WarningAction = "Continue"
+                        ErrorAction   = "Continue"
+                    }
+                    Set-Service @params
+                }
+                catch {
+                    throw $_.Exception.Message
+                }
+            }
+        }
+        break
     }
-    Uninstall-WindowsFeature @params
-}
-catch {
-    throw $_.Exception.Message
+    "Microsoft Windows 1* Enterprise for Virtual Desktops|Microsoft Windows 1* Enterprise|Microsoft Windows 1*" {
+        try {
+            $params = @{
+                FeatureName   = "Printing-XPSServices-Features", "SMB1Protocol", "WorkFolders-Client", `
+                    "FaxServicesClientPackage", "WindowsMediaPlayer", "MicrosoftWindowsPowerShellV2Root", `
+                    "MicrosoftWindowsPowerShellV2"
+                Online        = $true
+                NoRestart     = $true
+                WarningAction = "Continue"
+                ErrorAction   = "Continue"
+            }
+            Disable-WindowsOptionalFeature @params
+        }
+        catch {
+            throw $_.Exception.Message
+        }
+        
+        try {
+            $params = @{
+                Name                   = "Media.WindowsMediaPlayer~~~~0.0.12.0", "XPS.Viewer~~~~0.0.1.0", `
+                    "App.Support.QuickAssist~~~~0.0.1.0", "MathRecognizer~~~~0.0.1.0", `
+                    "Browser.InternetExplorer~~~~0.0.11.0", "Print.Fax.Scan~~~~0.0.1.0"
+                IncludeManagementTools = $true
+                WarningAction          = "Continue"
+                ErrorAction            = "Continue"
+            }
+            Uninstall-WindowsFeature @params
+        }
+        catch {
+            throw $_.Exception.Message
+        }
+
+        break
+    }
+    default {
+    }
 }
 
 # Enable services
