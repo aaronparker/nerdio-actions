83bcb2796b9f9b354b25f96c97d52126b2c0765a
Aaron Parker <aaron@stealthpuppy.com>
2022-11-28 10:24:28 +1100

    Update 13_Avd-Agents.ps1

--- avd/13_Avd-Agents.ps1
+++ avd/13_Avd-Agents.ps1
@@ -9,7 +9,7 @@
 New-Item -Path $Path -ItemType "Directory" -Force -ErrorAction "SilentlyContinue" | Out-Null
 
 # Run tasks/install apps
-#region RTC service
+#region Microsoft Remote Desktop WebRTC Redirector Service
 try {
     Import-Module -Name "Evergreen" -Force
     $App = Get-EvergreenApp -Name "MicrosoftWvdRtcService" | Where-Object { $_.Architecture -eq "x64" } | Select-Object -First 1
@@ -30,6 +30,29 @@ catch {
 }
 #endregion
 
+#region Microsoft Azure Virtual Desktop Multimedia Redirection Extensions
+try {
+    Import-Module -Name "Evergreen" -Force
+    $App = Get-EvergreenApp -Name "MicrosoftWvdMultimediaRedirection" | Where-Object { $_.Architecture -eq "x64" } | Select-Object -First 1
+    $OutFile = Save-EvergreenApp -InputObject $App -CustomPath $Path -WarningAction "SilentlyContinue"
+
+    # Install MMR
+    $params = @{
+        FilePath     = "$env:SystemRoot\System32\msiexec.exe"
+        ArgumentList = "/package `"$($OutFile.FullName)`" /quiet /log `"$env:ProgramData\NerdioManager\Logs\MicrosoftWvdMultimediaRedirection.log`""
+        NoNewWindow  = $true
+        Wait         = $true
+        PassThru     = $false
+    }
+    $result = Start-Process @params
+}
+catch {
+    throw "Exit code: $($result.ExitCode); Error: $($_.Exception.Message)"
+}
+#endregion
+
+
+# The items below should be installed automatically
 #region Boot Loader
 <#
 try {
