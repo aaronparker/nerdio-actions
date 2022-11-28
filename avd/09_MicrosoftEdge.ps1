84e72736dd1f210e17c3ce3847bd9ee09241f0cf
Aaron Parker <aaron@stealthpuppy.com>
2022-11-28 11:52:31 +1100

    Update 09_MicrosoftEdge.ps1

--- avd/09_MicrosoftEdge.ps1
+++ avd/09_MicrosoftEdge.ps1
@@ -69,7 +69,7 @@ catch {
 try {
     # Download
     Import-Module -Name "Evergreen" -Force
-    $App = Get-EvergreenApp -Name "MicrosoftEdgeWebView2" | Where-Object { $_.Architecture -eq "x64" -and $_.Channel -eq "Stable" } `
+    $App = Get-EvergreenApp -Name "MicrosoftEdgeWebView2Runtime" | Where-Object { $_.Architecture -eq "x64" -and $_.Channel -eq "Stable" } `
     | Sort-Object -Property @{ Expression = { [System.Version]$_.Version }; Descending = $true } | Select-Object -First 1
     $OutFile = Save-EvergreenApp -InputObject $App -CustomPath $Path -WarningAction "SilentlyContinue"
 }
