6ef1ac8a1b7a9dc3ec2c812a24ea38f3e303ba26
Aaron Parker <aaron@stealthpuppy.com>
2022-12-11 20:50:52 +1100

    Add draw.io, Firefox

---
+++ avd/411_draw.io.ps1
@@ -0,0 +1,38 @@
+#description: Installs the latest version of draw.io
+#execution mode: Combined
+#tags: Evergreen, draw.io
+#Requires -Modules Evergreen
+[System.String] $Path = "$env:SystemDrive\Apps\draw.io"
+
+#region Script logic
+New-Item -Path $Path -ItemType "Directory" -Force -ErrorAction "SilentlyContinue" | Out-Null
+New-Item -Path "$env:ProgramData\NerdioManager\Logs" -ItemType "Directory" -Force -ErrorAction "SilentlyContinue" | Out-Null
+
+try {
+    Import-Module -Name "Evergreen" -Force
+    $App = Invoke-EvergreenApp -Name "diagrams.net" | Where-Object { $_.Type -eq "msi" } | Select-Object -First 1
+    $OutFile = Save-EvergreenApp -InputObject $App -CustomPath $Path -WarningAction "SilentlyContinue"
+}
+catch {
+    throw $_
+}
+
+try {
+    $LogFile = "$env:ProgramData\NerdioManager\Logs\diagrams.net$($App.Version).log" -replace " ", ""
+    $params = @{
+        FilePath     = "$env:SystemRoot\System32\msiexec.exe"
+        ArgumentList = "/package `"$($OutFile.FullName)`" ALLUSERS=1 /quiet /log $LogFile"
+        NoNewWindow  = $true
+        Wait         = $true
+        PassThru     = $false
+    }
+    $result = Start-Process @params
+}
+catch {
+    throw "Exit code: $($result.ExitCode); Error: $($_.Exception.Message)"
+}
+
+Start-Sleep -Seconds 5
+$Shortcuts = @("$env:Public\Desktop\draw.io.lnk")
+Remove-Item -Path $Shortcuts -Force -ErrorAction "Ignore"
+#endregion
---
+++ avd/412_MozillaFirefox.ps1
@@ -0,0 +1,38 @@
+#description: Installs the latest version of Mozilla Firefox 64-bit with automatic update disabled
+#execution mode: Combined
+#tags: Evergreen, Mozilla, Firefox
+#Requires -Modules Evergreen
+[System.String] $Path = "$env:SystemDrive\Apps\Mozilla\Firefox"
+
+#region Script logic
+New-Item -Path $Path -ItemType "Directory" -Force -ErrorAction "SilentlyContinue" | Out-Null
+New-Item -Path "$env:ProgramData\NerdioManager\Logs" -ItemType "Directory" -Force -ErrorAction "SilentlyContinue" | Out-Null
+
+try {
+    Import-Module -Name "Evergreen" -Force
+    $App = Invoke-EvergreenApp -Name "MozillaFirefox" | Where-Object { $_.Channel -eq "LATEST_FIREFOX_VERSION" -and $_.Architecture -eq "x64" -and $_.Language -eq "en-US" -and $_.Type -eq "msi" } | Select-Object -First 1
+    $OutFile = Save-EvergreenApp -InputObject $App -CustomPath $Path -WarningAction "SilentlyContinue"
+}
+catch {
+    throw $_
+}
+
+try {
+    $LogFile = "$env:ProgramData\NerdioManager\Logs\MozillaFirefox$($App.Version).log" -replace " ", ""
+    $params = @{
+        FilePath     = "$env:SystemRoot\System32\msiexec.exe"
+        ArgumentList = "/package `"$($OutFile.FullName)`" DESKTOP_SHORTCUT=false TASKBAR_SHORTCUT=false INSTALL_MAINTENANCE_SERVICE=false REMOVE_DISTRIBUTION_DIR=true PREVENT_REBOOT_REQUIRED=true REGISTER_DEFAULT_AGENT=true /quiet /log $LogFile"
+        NoNewWindow  = $true
+        Wait         = $true
+        PassThru     = $false
+    }
+    $result = Start-Process @params
+}
+catch {
+    throw "Exit code: $($result.ExitCode); Error: $($_.Exception.Message)"
+}
+
+Start-Sleep -Seconds 5
+$Shortcuts = @("$env:Public\Desktop\Mozilla Firefox.lnk")
+Remove-Item -Path $Shortcuts -Force -ErrorAction "Ignore"
+#endregion
