8140189e22fa705db8731ccb85deee52226713a1
Aaron Parker <aaron@stealthpuppy.com>
2022-12-12 08:44:11 +1100

    Update module install

--- avd/011_SupportFunctions.ps1
+++ avd/011_SupportFunctions.ps1
@@ -5,16 +5,11 @@
 #region Script logic
 # Trust the PSGallery for modules
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-Install-PackageProvider -Name "NuGet" -MinimumVersion "2.8.5.208" -Force
-Install-PackageProvider -Name "PowerShellGet" -MinimumVersion "2.2.5" -Force
+Install-PackageProvider -Name "NuGet" -MinimumVersion "2.8.5.208"
+Install-PackageProvider -Name "PowerShellGet" -MinimumVersion "2.2.5"
 foreach ($Repository in "PSGallery") {
     if (Get-PSRepository | Where-Object { $_.Name -eq $Repository -and $_.InstallationPolicy -ne "Trusted" }) {
-        try {
-            Set-PSRepository -Name $Repository -InstallationPolicy "Trusted"
-        }
-        catch {
-            $_.Exception.Message
-        }
+        Set-PSRepository -Name $Repository -InstallationPolicy "Trusted"
     }
 }
 
@@ -22,22 +17,17 @@ foreach ($Repository in "PSGallery") {
 # Install the VcRedist module; https://docs.stealthpuppy.com/vcredist/
 foreach ($module in "Evergreen", "VcRedist", "PSWindowsUpdate") {
     $installedModule = Get-Module -Name $module -ListAvailable -ErrorAction "SilentlyContinue" | `
-        Sort-Object -Property @{ Expression = { [System.Version]$_.Version }; Descending = $true } | `
+        Sort-Object -Property @{ Expression = { [System.Version]$_.Version }; Descending = $true } -ErrorAction "SilentlyContinue" | `
         Select-Object -First 1
     $publishedModule = Find-Module -Name $module -ErrorAction "SilentlyContinue"
     if (($null -eq $installedModule) -or ([System.Version]$publishedModule.Version -gt [System.Version]$installedModule.Version)) {
-        try {
-            $params = @{
-                Name               = $module
-                SkipPublisherCheck = $true
-                Force              = $true
-                ErrorAction        = "Stop"
-            }
-            Install-Module @params
+        $params = @{
+            Name               = $module
+            SkipPublisherCheck = $true
+            Force              = $true
+            ErrorAction        = "Stop"
         }
-        catch {
-            throw $_.Exception.Message
-        }
+        Install-Module @params
     }
 }
 #endregion
--- tests/Install-Pester.ps1
+++ tests/Install-Pester.ps1
@@ -1,38 +1,26 @@
 <#
+    Install the latest version of Pester
 #>
 
 # Trust the PSGallery for modules
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-Install-PackageProvider -Name "NuGet" -MinimumVersion "2.8.5.208" -Force
-Install-PackageProvider -Name "PowerShellGet" -MinimumVersion "2.2.5" -Force
-foreach ($Repository in "PSGallery") {
-    if (Get-PSRepository | Where-Object { $_.Name -eq $Repository -and $_.InstallationPolicy -ne "Trusted" }) {
-        try {
-            Set-PSRepository -Name $Repository -InstallationPolicy "Trusted"
-        }
-        catch {
-            $_.Exception.Message
-        }
-    }
+Install-PackageProvider -Name "NuGet" -MinimumVersion "2.8.5.208"
+Install-PackageProvider -Name "PowerShellGet" -MinimumVersion "2.2.5"
+if (Get-PSRepository | Where-Object { $_.Name -eq $Repository -and $_.InstallationPolicy -ne "Trusted" }) {
+    Set-PSRepository -Name $Repository -InstallationPolicy "Trusted"
 }
 
 foreach ($module in "Pester") {
     $installedModule = Get-Module -Name $module -ListAvailable -ErrorAction "SilentlyContinue" | `
-        Sort-Object -Property @{ Expression = { [System.Version]$_.Version }; Descending = $true } | `
+        Sort-Object -Property @{ Expression = { [System.Version]$_.Version }; Descending = $true } -ErrorAction "SilentlyContinue" | `
         Select-Object -First 1
     $publishedModule = Find-Module -Name $module -ErrorAction "SilentlyContinue"
     if (($null -eq $installedModule) -or ([System.Version]$publishedModule.Version -gt [System.Version]$installedModule.Version)) {
-        try {
-            $params = @{
-                Name               = $module
-                SkipPublisherCheck = $true
-                Force              = $true
-                ErrorAction        = "Stop"
-            }
-            Install-Module @params
+        $params = @{
+            Name               = $module
+            SkipPublisherCheck = $true
+            ErrorAction        = "Stop"
         }
-        catch {
-            throw $_.Exception.Message
-        }
+        Install-Module @params
     }
 }
