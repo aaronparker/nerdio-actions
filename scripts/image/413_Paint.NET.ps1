8e7313d25c3d229686033be69c8cf423b5e21a66
Aaron Parker <aaron@stealthpuppy.com>
2022-12-17 22:42:10 +1100

    Add paint.net

---
+++ scripts/image/413_Paint.NET.ps1
@@ -0,0 +1,50 @@
+#description: Installs the latest version of Paint.NET 64-bit with automatic update disabled
+#execution mode: Combined
+#tags: Evergreen, Paint.NET
+#Requires -Modules Evergreen
+[System.String] $Path = "$env:SystemDrive\Apps\Paint.NET"
+
+#region Script logic
+New-Item -Path $Path -ItemType "Directory" -Force -ErrorAction "SilentlyContinue" | Out-Null
+New-Item -Path "$env:ProgramData\Evergreen\Logs" -ItemType "Directory" -Force -ErrorAction "SilentlyContinue" | Out-Null
+
+try {
+    Import-Module -Name "Evergreen" -Force
+    $App = Invoke-EvergreenApp -Name "PaintDotNetOfflineInstaller" | Where-Object { $_.Architecture -eq "x64" -and $_.URI -match "winmsi" } | Select-Object -First 1
+    $OutFile = Save-EvergreenApp -InputObject $App -CustomPath $Path -WarningAction "SilentlyContinue"
+}
+catch {
+    throw $_
+}
+
+try {
+    $params = @{
+        Path            = $OutFile.FullName
+        DestinationPath = $Path
+    }
+    Expand-Archive @params
+}
+catch {
+    throw $_
+}
+
+try {
+    $Installer = Get-ChildItem -Path $Path -Include "paint*.msi" -Recurse
+    $LogFile = "$env:ProgramData\Evergreen\Logs\Paint.NET$($App.Version).log" -replace " ", ""
+    $params = @{
+        FilePath     = "$env:SystemRoot\System32\msiexec.exe"
+        ArgumentList = "/package `"$($Installer.FullName)`" DESKTOPSHORTCUT=0 CHECKFORUPDATES=0 CHECKFORBETAS=0 /quiet /log $LogFile"
+        NoNewWindow  = $true
+        Wait         = $true
+        PassThru     = $false
+    }
+    $result = Start-Process @params
+}
+catch {
+    throw "Exit code: $($result.ExitCode); Error: $($_.Exception.Message)"
+}
+
+Start-Sleep -Seconds 5
+$Shortcuts = @("$env:Public\Desktop\Paint.NET.lnk")
+Remove-Item -Path $Shortcuts -Force -ErrorAction "Ignore"
+#endregion
---
+++ scripts/uninstall/Uninstall-Paint.NET.ps1
@@ -0,0 +1,55 @@
+#description: Uninstalls Paint.NET
+#execution mode: Combined
+#tags: Uninstall, Paint.NET
+
+#region Functions
+function Get-InstalledSoftware {
+    [OutputType([System.Object[]])]
+    [CmdletBinding()]
+    param ()
+    $UninstallKeys = @(
+        "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\*",
+        "HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*"
+    )
+    $Apps = @()
+    foreach ($Key in $UninstallKeys) {
+        try {
+            $propertyNames = "DisplayName", "DisplayVersion", "Publisher", "UninstallString", "PSPath", "WindowsInstaller", "InstallDate", "InstallSource", "HelpLink", "Language", "EstimatedSize", "SystemComponent"
+            $Apps += Get-ItemProperty -Path $Key -Name $propertyNames -ErrorAction "SilentlyContinue" | `
+                . { process { if ($null -ne $_.DisplayName) { $_ } } } | `
+                Where-Object { $_.SystemComponent -ne 1 } | `
+                Select-Object -Property @{n = "Name"; e = { $_.DisplayName } }, @{n = "Version"; e = { $_.DisplayVersion } }, "Publisher", "UninstallString", @{n = "RegistryPath"; e = { $_.PSPath -replace "Microsoft.PowerShell.Core\\Registry::", "" } }, "PSChildName", "WindowsInstaller", "InstallDate", "InstallSource", "HelpLink", "Language", "EstimatedSize" | `
+                Sort-Object -Property "DisplayName", "Publisher"
+        }
+        catch {
+            throw $_
+        }
+    }
+    return $Apps
+}
+#endregion
+
+#region Script logic
+New-Item -Path "$env:ProgramData\Evergreen\Logs" -ItemType "Directory" -Force -ErrorAction "SilentlyContinue" | Out-Null
+
+try {
+    $Apps = Get-InstalledSoftware | Where-Object { $_.Name -match "Paint.NET*" }
+    foreach ($App in $Apps) {
+        $LogFile = "$env:ProgramData\Evergreen\Logs\UninstallPaint.NET$($App.Version).log" -replace " ", ""
+        $params = @{
+            FilePath     = "$Env:SystemRoot\System32\msiexec.exe"
+            ArgumentList = "/uninstall `"$($App.PSChildName)`" /quiet /norestart /log $LogFile"
+            NoNewWindow  = $True
+            PassThru     = $True
+            Wait         = $True
+        }
+        $result = Start-Process @params
+    }
+}
+catch {
+    throw $_
+}
+finally {
+    exit $result.ExitCode
+}
+#endregion
