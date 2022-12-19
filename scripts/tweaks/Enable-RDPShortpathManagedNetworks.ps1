3cd886a135e0d1076bc371fde7372e1f626248a3
Aaron Parker <aaron@stealthpuppy.com>
2022-12-20 08:43:19 +1100

    Update scripts

--- scripts/tweaks/Enable-RDPShortpathManagedNetworks.ps1
+++ scripts/tweaks/Enable-RDPShortpathManagedNetworks.ps1
@@ -1,28 +1,28 @@
-#description: Enable RDP Shortpath on each session host VMs
+#description: Enable RDP Shortpath for managed networks. Reboot required.
 #execution mode: Combined
-#tags: Nerdio
+#tags: RDP Shortpath, Image
 <#
-Notes:
-This script will setup windows OS for shortpath as defined here: https://docs.microsoft.com/en-us/azure/virtual-desktop/shortpath
-A reboot is required after running this script for the configuration to take effect.
-Please read the MS Doc to ensure Direct line of sight and NSG rules are considered in your environment!
+https://docs.microsoft.com/en-us/azure/virtual-desktop/shortpath
 #>
 
 # Add registry keys
-$WinstationsKey = 'HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations'
-New-ItemProperty -Path $WinstationsKey -Name 'fUseUdpPortRedirector' -ErrorAction:SilentlyContinue -PropertyType:dword -Value 1 -Force
-New-ItemProperty -Path $WinstationsKey -Name 'UdpPortNumber' -ErrorAction:SilentlyContinue -PropertyType:dword -Value 3390 -Force
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations" /v "fUseUdpPortRedirector" /t "REG_DWORD" /d 1 /f | Out-Null
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations" /v "UdpPortNumber" /t "REG_DWORD" /d 3390 /f | Out-Null
 
-# Add windows firewall rule for shortpath RDP
-New-NetFirewallRule -DisplayName 'Remote Desktop - Shortpath (UDP-In)' `
-    -Action Allow `
-    -Description 'Inbound rule for the Remote Desktop service to allow RDP traffic. [UDP 3390]' `
-    -Group '@FirewallAPI.dll,-28752' `
-    -Name 'RemoteDesktop-UserMode-In-Shortpath-UDP' `
-    -PolicyStore PersistentStore `
-    -Profile Domain, Private `
-    -Service TermService `
-    -Protocol udp `
-    -LocalPort 3390 `
-    -Program '%SystemRoot%\system32\svchost.exe' `
-    -Enabled:True
+# Add windows firewall rule
+$params = @{
+    DisplayName = "Remote Desktop - RDP Shortpath (UDP-In)"
+    Action      = "Allow"
+    Description = "Inbound rule for the Remote Desktop service to allow RDP traffic. [UDP 3390]"
+    Group       = "@FirewallAPI.dll,-28752"
+    Name        = "RemoteDesktop-UserMode-In-Shortpath-UDP"
+    PolicyStore = "PersistentStore"
+    Profile      = "Domain, Private"
+    Service     = "TermService"
+    Protocol    = "udp"
+    LocalPort   = 3390
+    Program     = "$env:SystemRoot\system32\svchost.exe"
+    Enabled     = "true"
+    ErrorAction = "Stop"
+}
+New-NetFirewallRule @params
--- scripts/tweaks/Enable-RDPShortpathPublicNetworks.ps1
+++ scripts/tweaks/Enable-RDPShortpathPublicNetworks.ps1
@@ -1,18 +1,9 @@
-#description: Enable RDP Shortpath for public networks (Preview) on session host VMs
+#description: Enable RDP Shortpath for public networks. Reboot required.
 #execution mode: Combined
-#tags: Nerdio, Preview
+#tags: RDP Shortpath, Image, Preview
 <#
-Notes:
-This script will setup windows OS for RDP Shortpath for public networks as defined here: 
 https://docs.microsoft.com/en-us/azure/virtual-desktop/shortpath-public
-
-This feature is in preview, and it is recommended that you test in a validation environment
-before using in production.
-
-A reboot is required after running this script for the configuration to take effect.
 #>
 
 # Add registry keys
-
-$WinstationsKey = 'HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations'
-New-ItemProperty -Path $WinstationsKey -Name 'ICEControl' -ErrorAction:SilentlyContinue -PropertyType:dword -Value 2 -Force
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations" /v "ICEControl" /t "REG_DWORD" /d 2 /f | Out-Null
