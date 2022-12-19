46b5a6074d0315ff62fbb8897108dad800a6d1f4
Aaron Parker <aaron@stealthpuppy.com>
2022-12-19 18:39:39 +1100

    Create Set-IgnoreRemoteKeyboardLayout.ps1

---
+++ scripts/tweaks/Set-IgnoreRemoteKeyboardLayout.ps1
@@ -0,0 +1,6 @@
+#description: Ignore the keyboard layout of the endpoint and keep the selected input language in the AVD session host
+#execution mode: Combined
+#tags: Keyboard, Language, Image
+
+# Add the registry key
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\Keyboard Layout" /v "IgnoreRemoteKeyboardLayout" /d 1 /t "REG_DWORD" /f | Out-Null
