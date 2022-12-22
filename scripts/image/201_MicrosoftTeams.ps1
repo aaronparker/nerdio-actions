5433487dd741f905d60c55facd0e461ae03f1560
Aaron Parker <aaron@stealthpuppy.com>
2022-12-22 16:30:55 +1100

    Update 201_MicrosoftTeams.ps1
    Update uninstall logic

--- scripts/image/201_MicrosoftTeams.ps1
+++ scripts/image/201_MicrosoftTeams.ps1
@@ -5,6 +5,33 @@
 [System.String] $Path = "$env:SystemDrive\Apps\Microsoft\Teams"
 [System.String] $TeamsExe = "${env:ProgramFiles(x86)}\Microsoft\Teams\current\Teams.exe"
 
+#region Functions
+function Get-InstalledSoftware {
+    [OutputType([System.Object[]])]
+    [CmdletBinding()]
+    param ()
+    $UninstallKeys = @(
+        "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\*",
+        "HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*"
+    )
+    $Apps = @()
+    foreach ($Key in $UninstallKeys) {
+        try {
+            $propertyNames = "DisplayName", "DisplayVersion", "Publisher", "UninstallString", "PSPath", "WindowsInstaller", "InstallDate", "InstallSource", "HelpLink", "Language", "EstimatedSize", "SystemComponent"
+            $Apps += Get-ItemProperty -Path $Key -Name $propertyNames -ErrorAction "SilentlyContinue" | `
+                . { process { if ($null -ne $_.DisplayName) { $_ } } } | `
+                Where-Object { $_.SystemComponent -ne 1 } | `
+                Select-Object -Property @{n = "Name"; e = { $_.DisplayName } }, @{n = "Version"; e = { $_.DisplayVersion } }, "Publisher", "UninstallString", @{n = "RegistryPath"; e = { $_.PSPath -replace "Microsoft.PowerShell.Core\\Registry::", "" } }, "PSChildName", "WindowsInstaller", "InstallDate", "InstallSource", "HelpLink", "Language", "EstimatedSize" | `
+                Sort-Object -Property "DisplayName", "Publisher"
+        }
+        catch {
+            throw $_
+        }
+    }
+    return $Apps
+}
+#endregion
+
 #region Script logic
 New-Item -Path $Path -ItemType "Directory" -Force -ErrorAction "SilentlyContinue" | Out-Null
 New-Item -Path "$env:ProgramData\Evergreen\Logs" -ItemType "Directory" -Force -ErrorAction "SilentlyContinue" | Out-Null
@@ -41,32 +68,40 @@ try {
             Remove-Item -Path $Folders -Recurse -Force -ErrorAction "Ignore"
         }
     }
+
+    $Apps = Get-InstalledSoftware | Where-Object { $_.Name -match "Teams Machine-Wide Installer" }
+    foreach ($App in $Apps) {
+        $LogFile = "$env:ProgramData\Evergreen\Logs\UninstallGoogleChrome$($App.Version).log" -replace " ", ""
+        $params = @{
+            FilePath     = "$Env:SystemRoot\System32\msiexec.exe"
+            ArgumentList = "/uninstall `"$($App.PSChildName)`" /quiet /norestart /log $LogFile"
+            NoNewWindow  = $True
+            PassThru     = $True
+            Wait         = $True
+        }
+        $result = Start-Process @params
+    }
 }
 catch {
     throw "Exit code: $($result.ExitCode); Error: $($_.Exception.Message)"
 }
 
-if (Test-Path -Path $TeamsExe) {
-    # Teams is installed
+try {
+    # Install Teams
+    New-Item -Path "HKLM:\SOFTWARE\Microsoft\Teams" -Force -ErrorAction "SilentlyContinue" | Out-Null
+    New-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Teams" -Name "IsWVDEnvironment" -PropertyType "DWORD" -Value 1 -Force -ErrorAction "SilentlyContinue" | Out-Null
+    $LogFile = $LogFile = "$env:ProgramData\Evergreen\Logs\MicrosoftTeams$($App.Version).log" -replace " ", ""
+    $params = @{
+        FilePath     = "$env:SystemRoot\System32\msiexec.exe"
+        ArgumentList = "/package $($OutFile.FullName) OPTIONS=`"noAutoStart=true`" ALLUSER=1 ALLUSERS=1 /quiet /log $LogFile"
+        NoNewWindow  = $true
+        Wait         = $true
+        PassThru     = $false
+    }
+    $result = Start-Process @params
 }
-else {
-    try {
-        # Install Teams
-        New-Item -Path "HKLM:\SOFTWARE\Microsoft\Teams" -Force -ErrorAction "SilentlyContinue" | Out-Null
-        New-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Teams" -Name "IsWVDEnvironment" -PropertyType "DWORD" -Value 1 -Force -ErrorAction "SilentlyContinue" | Out-Null
-        $LogFile = $LogFile = "$env:ProgramData\Evergreen\Logs\MicrosoftTeams$($App.Version).log" -replace " ", ""
-        $params = @{
-            FilePath     = "$env:SystemRoot\System32\msiexec.exe"
-            ArgumentList = "/package $($OutFile.FullName) OPTIONS=`"noAutoStart=true`" ALLUSER=1 ALLUSERS=1 /quiet /log $LogFile"
-            NoNewWindow  = $true
-            Wait         = $true
-            PassThru     = $false
-        }
-        $result = Start-Process @params
-    }
-    catch {
-        throw "Exit code: $($result.ExitCode); Error: $($_.Exception.Message)"
-    }
+catch {
+    throw "Exit code: $($result.ExitCode); Error: $($_.Exception.Message)"
 }
 
 try {
