2fe814c7178d7df3b243dfaf87be398f36ed798a
Aaron Parker <aaron@stealthpuppy.com>
2023-01-17 14:15:35 +1100

    Add ShareX

---
+++ scripts/image/416_ShareX.ps1
@@ -0,0 +1,44 @@
+#description: Installs the latest version of ShareX
+#execution mode: Combined
+#tags: Evergreen, ShareX
+#Requires -Modules Evergreen
+[System.String] $Path = "$Env:SystemDrive\Apps\ShareX"
+
+#region Script logic
+New-Item -Path $Path -ItemType "Directory" -Force -ErrorAction "SilentlyContinue" | Out-Null
+New-Item -Path "$Env:ProgramData\Evergreen\Logs" -ItemType "Directory" -Force -ErrorAction "SilentlyContinue" | Out-Null
+
+try {
+    Import-Module -Name "Evergreen" -Force
+    $App = Get-EvergreenApp -Name "ShareX" | Where-Object { $_.Type -eq "exe" } | Select-Object -First 1
+    $OutFile = Save-EvergreenApp -InputObject $App -CustomPath $Path -WarningAction "SilentlyContinue"
+}
+catch {
+    throw $_
+}
+
+try {
+    $LogFile = "$Env:ProgramData\Evergreen\Logs\ShareX$($App.Version).log" -replace " ", ""
+    $params = @{
+        FilePath     = $OutFile.FullName
+        ArgumentList = "/SP- /VERYSILENT /SUPPRESSMSGBOXES /NORESTART /TASKS= /FORCECLOSEAPPLICATIONS /LOGCLOSEAPPLICATIONS /NORESTARTAPPLICATIONS /LOG=$LogFile"
+        NoNewWindow  = $true
+        Wait         = $false
+        PassThru     = $true
+        ErrorAction  = "Continue"
+    }
+    $result = Start-Process @params
+    $result.ExitCode
+}
+catch {
+    throw $_
+}
+
+Start-Sleep -Seconds 5
+Get-Process -ErrorAction "SilentlyContinue" | `
+    Where-Object { $_.Path -like "$Env:ProgramFiles\ShareX\*" } | `
+    Stop-Process -Force -ErrorAction "SilentlyContinue"
+$Shortcuts = @("$Env:Public\Desktop\ShareX.lnk",
+    "$Env:ProgramData\Microsoft\Windows\Start Menu\Programs\ShareX\Uninstall ShareX.lnk")
+Remove-Item -Path $Shortcuts -Force -ErrorAction "Ignore"
+#endregion
---
+++ scripts/uninstall/Uninstall-ShareX.ps1
@@ -0,0 +1,32 @@
+#description: Uninstalls ShareX
+#execution mode: Combined
+#tags: Uninstall, ShareX
+
+#region Script logic
+try {
+    Get-Process -ErrorAction "SilentlyContinue" | `
+        Where-Object { $_.Path -like "$Env:ProgramFiles\ShareX\*" } | `
+        Stop-Process -Force -ErrorAction "SilentlyContinue"
+}
+catch {
+    Write-Warning -Message "Failed to stop ShareX processes."
+}
+
+if (Test-Path -Path "$Env:ProgramFiles\ShareX\unins000.exe") {
+    $params = @{
+        FilePath     = "$Env:ProgramFiles\ShareX\unins000.exe"
+        ArgumentList = "/VERYSILENT"
+        NoNewWindow  = $True
+        PassThru     = $True
+        Wait         = $True
+        ErrorAction  = "Continue"
+    }
+    $result = Start-Process @params
+    $result.ExitCode
+    if ($result.ExitCode -eq 0) {
+        if (Test-Path -Path "$Env:ProgramFiles\ShareX") {
+            Remove-Item -Path "$Env:ProgramFiles\ShareX" -Recurse -Force -ErrorAction "SilentlyContinue"
+        }
+    }
+}
+#endregion
