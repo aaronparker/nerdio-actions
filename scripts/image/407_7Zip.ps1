aaddfb3a295d214a6bc7a0f759a7b9fd9bdfdf82
Aaron Parker <aaron@stealthpuppy.com>
2023-03-14 10:25:22 +1100

    Update 7zip file associations

--- scripts/image/407_7Zip.ps1
+++ scripts/image/407_7Zip.ps1
@@ -32,4 +32,64 @@ try {
 catch {
     throw $_.Exception.Message
 }
+
+# Add registry entries for additional file types
+@"
+Windows Registry Editor Version 5.00
+
+[HKEY_CLASSES_ROOT\.7z]
+@="7-Zip-Zstandard.7z"
+
+[HKEY_CLASSES_ROOT\7-Zip-Zstandard.7z]
+@="7z Archive"
+
+[HKEY_CLASSES_ROOT\7-Zip-Zstandard.7z\DefaultIcon]
+@="C:\\Program Files\\7-Zip-Zstandard\\7z.dll,0"
+
+[HKEY_CLASSES_ROOT\7-Zip-Zstandard.7z\shell]
+@=""
+
+[HKEY_CLASSES_ROOT\7-Zip-Zstandard.7z\shell\open]
+@=""
+
+[HKEY_CLASSES_ROOT\7-Zip-Zstandard.7z\shell\open\command]
+@="\"C:\\Program Files\\7-Zip-Zstandard\\7zFM.exe\" \"%1\""
+
+[HKEY_CLASSES_ROOT\7-Zip-Zstandard.rar]
+@="rar Archive"
+
+[HKEY_CLASSES_ROOT\7-Zip-Zstandard.rar\DefaultIcon]
+@="C:\\Program Files\\7-Zip-Zstandard\\7z.dll,3"
+
+[HKEY_CLASSES_ROOT\7-Zip-Zstandard.rar\shell]
+@=""
+
+[HKEY_CLASSES_ROOT\7-Zip-Zstandard.rar\shell\open]
+@=""
+
+[HKEY_CLASSES_ROOT\7-Zip-Zstandard.rar\shell\open\command]
+@="\"C:\\Program Files\\7-Zip-Zstandard\\7zFM.exe\" \"%1\""
+
+[HKEY_CLASSES_ROOT\7-Zip-Zstandard.zip]
+@="zip Archive"
+
+[HKEY_CLASSES_ROOT\7-Zip-Zstandard.zip\DefaultIcon]
+@="C:\\Program Files\\7-Zip-Zstandard\\7z.dll,1"
+
+[HKEY_CLASSES_ROOT\7-Zip-Zstandard.zip\shell]
+@=""
+
+[HKEY_CLASSES_ROOT\7-Zip-Zstandard.zip\shell\open]
+@=""
+
+[HKEY_CLASSES_ROOT\7-Zip-Zstandard.zip\shell\open\command]
+@="\"C:\\Program Files\\7-Zip-Zstandard\\7zFM.exe\" \"%1\""
+
+[HKEY_CLASSES_ROOT\.rar]
+@="7-Zip-Zstandard.rar"
+
+[HKEY_CLASSES_ROOT\.zip]
+@="7-Zip-Zstandard.zip"
+"@ | Out-File -FilePath "$Path\FileTypes.reg" -Encoding "bigendianunicode" -Force
+if (Test-Path -Path "$Path\FileTypes.reg") { & "$Env:SystemRoot\System32\reg.exe" import "$Path\FileTypes.reg" /reg:64 }
 #endregion
