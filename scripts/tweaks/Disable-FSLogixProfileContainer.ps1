20d2a338ac8d6ed4225c47d02f04524b5d24d2e0
Aaron Parker <aaron@stealthpuppy.com>
2023-03-15 11:56:49 +1100

    Update Disable-FSLogixProfileContainer.ps1
    Fix reg add

--- scripts/tweaks/Disable-FSLogixProfileContainer.ps1
+++ scripts/tweaks/Disable-FSLogixProfileContainer.ps1
@@ -4,4 +4,4 @@
 
 # https://learn.microsoft.com/en-us/fslogix/reference-configuration-settings?tabs=profiles#enabled
 
-reg delete "HKLM\SOFTWARE\FSLogix\Profiles" /v "Enabled" /t "REG_DWORD" /d 0 /f | Out-Null
+reg add "HKLM\SOFTWARE\FSLogix\Profiles" /v "Enabled" /t "REG_DWORD" /d 0 /f | Out-Null
