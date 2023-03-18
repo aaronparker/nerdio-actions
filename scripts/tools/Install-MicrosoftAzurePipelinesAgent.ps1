1312f121d5138124222996b40b6f0ff4a0a6cbbc
Aaron Parker <aaron@stealthpuppy.com>
2023-03-19 10:00:32 +1100

    Create Install-MicrosoftAzurePipelinesAgent.ps1

---
+++ scripts/tools/Install-MicrosoftAzurePipelinesAgent.ps1
@@ -0,0 +1,40 @@
+#description: Installs the Microsoft Azure Pipelines agent to enable automated testing via Azure Pipelines. Do not run on production session hosts.
+#execution mode: Combined
+#tags: Evergreen, Testing
+#Requires -Modules Evergreen
+[System.String] $Path = "$Env:SystemDrive\agents"
+
+# Check that the required variables have been set in Nerdio Manager
+foreach ($Value in "DevOpsUrl", "DevOpsPat", "DevOpsPool") {
+    if ($null -eq $SecureVars.$Value) { throw "$Value is $null"}
+}
+
+#region Script logic
+New-Item -Path $Path -ItemType "Directory" -Force -ErrorAction "SilentlyContinue" | Out-Null
+
+try {
+    # Download
+    Import-Module -Name "Evergreen" -Force
+    $App = Invoke-EvergreenApp -Name "MicrosoftAzurePipelinesAgent" | Select-Object -First 1
+    $OutFile = Save-EvergreenApp -InputObject $App -CustomPath $Env:Temp -WarningAction "SilentlyContinue"
+}
+catch {
+    throw $_.Exception.Message
+}
+
+try {
+    Expand-Archive -Path $OutFile.FullName -DestinationPath $Path -Force
+    Push-Location -Path $Path
+    $params = @{
+        FilePath    = "$Path\config.cmd"
+        Arguments   = "--unattended --url $($SecureVars.DevOpsUrl) --auth pat --token `"$($SecureVars.DevOpsPat)`" --pool `"$($SecureVars.DevOpsPool)`" --agent $Env:COMPUTERNAME --runAsService --replace"
+        Wait        = $true
+        WindowStyle = "hidden"
+    }
+    $result = Start-Process @params
+    Write-Information -MessageData ":: Install exit code: $($result.ExitCode)" -InformationAction "Continue"
+}
+catch {
+    throw $_
+}
+#endregion
