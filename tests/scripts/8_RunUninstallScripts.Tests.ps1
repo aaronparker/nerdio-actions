239537b9ff7f6a25815f200de14c000a870e441c
Aaron Parker <aaron@stealthpuppy.com>
2023-07-20 10:47:33 +1000

    Exclude scripts in test

--- tests/scripts/8_RunUninstallScripts.Tests.ps1
+++ tests/scripts/8_RunUninstallScripts.Tests.ps1
@@ -19,7 +19,7 @@ BeforeDiscovery {
 
     # Get the scripts to test
     $Scripts = Get-ChildItem -Path $Path -Include "*.ps1" -Recurse -Exclude @("Uninstall-NotepadPlusPlus.ps1")
-    $Scripts2 = Get-ChildItem -Path $Path -Include "*.ps1" -Recurse -Exclude @("Uninstall-7ZipZS.ps1", "Uninstall-MicrosoftNET.ps1", "Uninstall-NotepadPlusPlus.ps1")
+    $Scripts2 = Get-ChildItem -Path $Path -Include "*.ps1" -Recurse -Exclude @("Uninstall-7ZipZS.ps1", "Uninstall-MicrosoftNET.ps1", "Uninstall-NotepadPlusPlus.ps1", "Uninstall-MicrosoftOneDrive.ps1")
 }
 
 Describe "Uninstall scripts with software installed" {
