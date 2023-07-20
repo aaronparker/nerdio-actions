81155243d4a4e84dd4961b5b4ee720da66db3578
Aaron Parker <aaron@stealthpuppy.com>
2023-07-20 20:37:32 +1000

    Update 499_CrowdStrikeWindowsSensor.ps1

--- scripts/image/499_CrowdStrikeWindowsSensor.ps1
+++ scripts/image/499_CrowdStrikeWindowsSensor.ps1
@@ -1,4 +1,4 @@
-#description: Downloads and installs the CrowdStrike Windows Sensor from a specified URL
+#description: Downloads and installs the CrowdStrike Windows Sensor from a specified URL. Run Scripted actions when host VM is STARTED. 
 #execution mode: Combined
 #tags: CrowdStrike
 #Requires -Modules Evergreen
@@ -24,7 +24,7 @@ try {
     # Install the agent
     $params = @{
         FilePath     = $OutFile.FullName
-        ArgumentList = "/install /quiet /norestart /log `"$Env:ProgramData\Nerdio\Logs`" CID=$($SecureVars.CrowdStrikeCID) VDI=1 NO_START=1"
+        ArgumentList = "/install /quiet /norestart /log `"$Env:ProgramData\Nerdio\Logs`" CID=$($SecureVars.CrowdStrikeCID) VDI=1" # NO_START=1
         NoNewWindow  = $true
         Wait         = $true
         PassThru     = $true
