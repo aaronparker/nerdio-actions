dc6fd8e4a2fa9b92ced5fcd48847dc3d53efc76c
Aaron Parker <aaron@stealthpuppy.com>
2023-08-05 20:50:10 +1000

    Add MicrosoftSsms

---
+++ scripts/image/212_MicrosoftSQLServerManagementStudio.ps1
@@ -0,0 +1,38 @@
+#description: Installs the latest Microsoft SQL Server Management Studio
+#execution mode: Combined
+#tags: Evergreen, Microsoft, SQL Server
+#Requires -Modules Evergreen
+[System.String] $Path = "$Env:SystemDrive\Apps\Microsoft\Ssms"
+
+#region Script logic
+New-Item -Path $Path -ItemType "Directory" -Force -ErrorAction "SilentlyContinue" | Out-Null
+New-Item -Path "$Env:ProgramData\Nerdio\Logs" -ItemType "Directory" -Force -ErrorAction "SilentlyContinue" | Out-Null
+
+try {
+    Import-Module -Name "Evergreen" -Force
+    $App = Invoke-EvergreenApp -Name "MicrosoftSsms" | `
+        Where-Object { $_.Language -eq "English" } | Select-Object -First 1
+    $OutFile = Save-EvergreenApp -InputObject $App -CustomPath $Path -WarningAction "SilentlyContinue"
+}
+catch {
+    throw $_.Exception.Message
+}
+
+try {
+    Write-Information -MessageData ":: Install Microsoft SQL Server Management Studio" -InformationAction "Continue"
+    $LogFile = "$Env:ProgramData\Nerdio\Logs\MicrosoftSQLServerManagementStudio$($App.Version).log" -replace " ", ""
+    $params = @{
+        FilePath     = $OutFile.FullName
+        ArgumentList = "/install /quiet /norestart DoNotInstallAzureDataStudio=1 /log $LogFile"
+        NoNewWindow  = $true
+        Wait         = $true
+        PassThru     = $true
+        ErrorAction  = "Continue"
+    }
+    $result = Start-Process @params
+    Write-Information -MessageData ":: Install exit code: $($result.ExitCode)" -InformationAction "Continue"
+}
+catch {
+    throw $_.Exception.Message
+}
+#endregion
