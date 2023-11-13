7f991a3f4fb2c475cea58c3b6fdace014a8ec67b
Aaron Parker <aaron@stealthpuppy.com>
2023-11-14 10:00:20 +1100

    Update scripts

--- scripts/image/000_PrepImage.ps1
+++ scripts/image/000_PrepImage.ps1
@@ -3,11 +3,6 @@
 #tags: Image
 
 try {
-    if ((Get-MpPreference).DisableRealtimeMonitoring -eq $false) {
-        # Microsoft Defender (may not work on current versions)
-        Set-MpPreference -DisableRealtimeMonitoring $true
-    }
-
     if ((Get-CimInstance -ClassName "CIM_OperatingSystem").Caption -like "Microsoft Windows 1*") {
         # Prevent Windows from installing stuff during deployment
         Write-Information -MessageData ":: Set policy to prevent updates during deployment" -InformationAction "Continue"
@@ -20,9 +15,6 @@ try {
 
     # Enable time zone redirection
     reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows NT\Terminal Services" /v fEnableTimeZoneRedirection /t REG_DWORD /d 1 /f
-
-    # Disable Storage Sense
-    # reg add "HKLM\Software\Microsoft\Windows\CurrentVersion\StorageSense\Parameters\StoragePolicy" /v 01 /t REG_DWORD /d 0 /f
 
     # Create logs directory and compress
     Write-Information -MessageData ":: Create and compress: '$Env:ProgramData\Nerdio\Logs'" -InformationAction "Continue"
--- scripts/image/999_CleanupImage.ps1
+++ scripts/image/999_CleanupImage.ps1
@@ -1,33 +1,30 @@
 #description: Reenables settings, removes application installers, and remove logs older than 30 days post image completion
 #execution mode: Combined
 #tags: Image
-[System.String] $Path = "$Env:SystemDrive\Apps"
 
 try {
-    if ((Get-MpPreference).DisableRealtimeMonitoring -eq $true) {
-        # Re-enable Defender
-        Set-MpPreference -DisableRealtimeMonitoring $false
-    }
-
     if ((Get-CimInstance -ClassName "CIM_OperatingSystem").Caption -like "Microsoft Windows 1*") {
         # Remove policies
         Write-Information -MessageData ":: Remove policies that prevent updates during deployment" -InformationAction "Continue"
-        # reg delete "HKLM\Software\Policies\Microsoft\Windows\CloudContent" /v "DisableWindowsConsumerFeatures" /f
         reg delete "HKLM\Software\Policies\Microsoft\WindowsStore" /v "AutoDownload" /f
     }
-
-    # Disable Windows Update
-    reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU" /v "NoAutoUpdate" /t REG_DWORD /d 1 /f
-
-    # Remove C:\Apps folder
-    if (Test-Path -Path $Path) { Remove-Item -Path $Path -Recurse -Force -ErrorAction "SilentlyContinue" }
-    if (Test-Path -Path "$env:Temp") { Remove-Item -Path "$env:Temp" -Recurse -Force -ErrorAction "SilentlyContinue" }
 }
 catch {
     throw $_
 }
 
+# Remove paths that we should not need to leave around in the image
+if (Test-Path -Path "$Env:SystemDrive\Apps") { Remove-Item -Path "$Env:SystemDrive\Apps" -Recurse -Force -ErrorAction "SilentlyContinue" }
+if (Test-Path -Path "$Env:SystemDrive\DeployAgent") { Remove-Item -Path "$Env:SystemDrive\DeployAgent" -Recurse -Force -ErrorAction "SilentlyContinue" }
+
+# Remove items from the Temp directory (note that scripts run as SYSTEM)
+Get-ChildItem -Path "$Env:Temp" -Include "*.*" -Recurse | `
+    Remove-Item -Force -ErrorAction "Ignore"
+
 # Remove logs older than 30 days
 Get-ChildItem -Path "$Env:ProgramData\Nerdio\Logs" -Include "*.*" -Recurse | `
     Where-Object { ($_.LastWriteTime -lt (Get-Date).AddDays(-30)) -and ($_.psIsContainer -eq $false) } | `
     Remove-Item -Force -ErrorAction "Ignore"
+
+# Disable Windows Update
+# reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU" /v "NoAutoUpdate" /t REG_DWORD /d 1 /f
