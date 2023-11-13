1de82021299102eaf6e7e5a33dc318c6b5e9e159
Aaron Parker <aaron@stealthpuppy.com>
2023-11-13 13:26:27 +1100

    Update Invoke-ImageOptimise.ps1

--- opitimise/Invoke-ImageOptimise.ps1
+++ opitimise/Invoke-ImageOptimise.ps1
@@ -1,4 +1,6 @@
-# Basic optimisations for non-desktop images
+<#
+    Basic optimisations for non-desktop images
+#>
 
 # Disable consumer features
 reg add "HKLM\Software\Policies\Microsoft\Windows\CloudContent" /v "DisableWindowsConsumerFeatures" /d 1 /t "REG_DWORD" /f | Out-Null
@@ -9,8 +11,36 @@ reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows NT\Terminal Services" /v fEnab
 # Delete the policy setting created by MDT
 reg delete "HKLM\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU" /f | Out-Null
 
+# Trust the PSGallery for modules
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+Install-PackageProvider -Name "NuGet" -MinimumVersion "2.8.5.208" -Force -ErrorAction "SilentlyContinue"
+Install-PackageProvider -Name "PowerShellGet" -MinimumVersion "2.2.5" -Force -ErrorAction "SilentlyContinue"
+foreach ($Repository in "PSGallery") {
+    if (Get-PSRepository | Where-Object { $_.Name -eq $Repository -and $_.InstallationPolicy -ne "Trusted" }) {
+        Set-PSRepository -Name $Repository -InstallationPolicy "Trusted"
+    }
+}
+
+# Install the Evergreen module; https://github.com/aaronparker/Evergreen
+# Install the VcRedist module; https://docs.stealthpuppy.com/vcredist/
+foreach ($module in "Evergreen", "PSWindowsUpdate") {
+    $installedModule = Get-Module -Name $module -ListAvailable -ErrorAction "SilentlyContinue" | `
+        Sort-Object -Property @{ Expression = { [System.Version]$_.Version }; Descending = $true } -ErrorAction "SilentlyContinue" | `
+        Select-Object -First 1
+    $publishedModule = Find-Module -Name $module -ErrorAction "SilentlyContinue"
+    if (($null -eq $installedModule) -or ([System.Version]$publishedModule.Version -gt [System.Version]$installedModule.Version)) {
+        Write-Information -MessageData ":: Install module: $module" -InformationAction "Continue"
+        $params = @{
+            Name               = $module
+            SkipPublisherCheck = $true
+            Force              = $true
+            ErrorAction        = "Stop"
+        }
+        Install-Module @params
+    }
+}
+
 # Install updates
-Install-Module -Name "WindowsUpdate"
 Import-Module -Name "PSWindowsUpdate"
 Install-WindowsUpdate -AcceptAll -MicrosoftUpdate -IgnoreReboot -IgnoreRebootRequired | Select-Object -Property "Title", "Size"
 
@@ -29,13 +59,15 @@ Set-SystemPreferredUILanguage -Language $Language
 
 # Remove the FSLogix agent
 $FsLogix = Get-ChildItem -Path "$Env:ProgramData\Package Cache" -Recurse -Include "FSLogixAppsSetup.exe"
-$params = @{
-    FilePath     = $FsLogix.FullName
-    ArgumentList = "/norestart /uninstall /quiet"
-    Wait         = $true
-    ErrorAction  = "Continue"
+foreach ($File in $FsLogix) {
+    $params = @{
+        FilePath     = $File.FullName
+        ArgumentList = "/norestart /uninstall /quiet"
+        Wait         = $true
+        ErrorAction  = "Continue"
+    }
+    Start-Process @params
 }
-Start-Process @params
 
 #region Remove inbox apps
 $PackageFamilyNameBlockList = @(
@@ -55,9 +87,6 @@ $PackageFamilyNameBlockList = @(
     "Microsoft.HEIFImageExtension_8wekyb3d8bbwe",
     "Microsoft.Messaging_8wekyb3d8bbwe",
     "Microsoft.Microsoft3DViewer_8wekyb3d8bbwe",
-    # "Microsoft.MicrosoftAccessoryCenter_8wekyb3d8bbwe",
-    # "Microsoft.MicrosoftEdge.Stable_8wekyb3d8bbwe",
-    # "Microsoft.MicrosoftJournal_8wekyb3d8bbwe",
     "Microsoft.MicrosoftOfficeHub_8wekyb3d8bbwe",
     "Microsoft.MicrosoftSolitaireCollection_8wekyb3d8bbwe",
     "Microsoft.MicrosoftStickyNotes_8wekyb3d8bbwe",
@@ -71,13 +100,9 @@ $PackageFamilyNameBlockList = @(
     "Microsoft.Office.Desktop.Publisher_8wekyb3d8bbwe",
     "Microsoft.Office.Desktop.Word_8wekyb3d8bbwe",
     "Microsoft.OneConnect_8wekyb3d8bbwe",
-    # "Microsoft.OneDriveSync_8wekyb3d8bbwe",
-    # "Microsoft.Paint_8wekyb3d8bbwe",
     "Microsoft.People_8wekyb3d8bbwe",
-    # "Microsoft.PPIProjection_cw5n1h2txyewy",
     "Microsoft.PowerAutomateDesktop_8wekyb3d8bbwe",
     "Microsoft.Print3D_8wekyb3d8bbwe",
-    # "Microsoft.ScreenSketch_8wekyb3d8bbwe",
     "Microsoft.SkypeApp_kzf8qxf38zg5c",
     "Microsoft.Todos_8wekyb3d8bbwe",
     "Microsoft.VP9VideoExtensions_8wekyb3d8bbwe",
@@ -85,22 +110,18 @@ $PackageFamilyNameBlockList = @(
     "Microsoft.WebpImageExtension_8wekyb3d8bbwe",
     "Microsoft.Windows.Photos_8wekyb3d8bbwe",
     "Microsoft.WindowsAlarms_8wekyb3d8bbwe",
-    # "Microsoft.WindowsCalculator_8wekyb3d8bbwe",
     "Microsoft.WindowsCamera_8wekyb3d8bbwe",
     "Microsoft.windowscommunicationsapps_8wekyb3d8bbwe",
     "Microsoft.WindowsFeedbackHub_8wekyb3d8bbwe",
     "Microsoft.WindowsMaps_8wekyb3d8bbwe",
-    # "Microsoft.WindowsNotepad_8wekyb3d8bbwe",
     "Microsoft.WindowsPhone_8wekyb3d8bbwe",
     "Microsoft.WindowsSoundRecorder_8wekyb3d8bbwe",
-    # "Microsoft.WindowsTerminal_8wekyb3d8bbwe",
     "Microsoft.XboxApp_8wekyb3d8bbwe",
     "Microsoft.XboxGameOverlay_8wekyb3d8bbwe",
     "Microsoft.XboxGamingOverlay_8wekyb3d8bbwe",
     "Microsoft.YourPhone_8wekyb3d8bbwe",
     "Microsoft.ZuneMusic_8wekyb3d8bbwe",
     "Microsoft.ZuneVideo_8wekyb3d8bbwe",
-    # "MicrosoftCorporationII.QuickAssist_8wekyb3d8bbwe",
     "MicrosoftTeams_8wekyb3d8bbwe",
     "Disney.37853FC22B2CE_6rarf9sa4v8jt",
     "SpotifyAB.SpotifyMusic_zpdnekdrzrea0"
@@ -115,7 +136,7 @@ foreach ($app in $PackageFamilyNameBlockList) {
 #endregion
 
 # Install RDP connection apps
-Install-Module -Name "Evergreen"
+Import-Module -Name "Evergreen"
 [System.String] $Path = "$Env:ProgramFiles\RemoteDisplayAnalyzer"
 New-Item -Path $Path -ItemType "Directory" -Force -ErrorAction "SilentlyContinue" | Out-Null
 
