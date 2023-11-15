d1e0d92b5b3ea3527c050cd85dcb73d049642306
Aaron Parker <aaron@stealthpuppy.com>
2023-11-15 16:47:30 +1100

    Create 213_MicrosoftAzureVirtualDesktopClient.ps1

---
+++ scripts/image/213_MicrosoftAzureVirtualDesktopClient.ps1
@@ -0,0 +1,38 @@
+#description: Installs the latest Microsoft Azure Virtual Desktop Remote Desktop client
+#execution mode: Combined
+#tags: Evergreen, Microsoft, Remote Desktop
+#Requires -Modules Evergreen
+[System.String] $Path = "$Env:SystemDrive\Apps\Microsoft\Avd"
+
+#region Script logic
+New-Item -Path $Path -ItemType "Directory" -Force -ErrorAction "SilentlyContinue" | Out-Null
+New-Item -Path "$Env:ProgramData\Nerdio\Logs" -ItemType "Directory" -Force -ErrorAction "SilentlyContinue" | Out-Null
+
+try {
+    Import-Module -Name "Evergreen" -Force
+    $App = Get-EvergreenApp -Name "MicrosoftWvdRemoteDesktop" | `
+        Where-Object { $_.Architecture -eq "x64" -and $_.Channel -eq "Public" } | Select-Object -First 1
+    $OutFile = Save-EvergreenApp -InputObject $App -CustomPath $Path -WarningAction "SilentlyContinue"
+}
+catch {
+    throw $_
+}
+
+try {
+    Write-Information -MessageData ":: Install Microsoft Azure Virtual Desktop Remote Desktop client" -InformationAction "Continue"
+    $LogFile = "$Env:ProgramData\Nerdio\Logs\MicrosoftAvdClient$($App.Version).log" -replace " ", ""
+    $params = @{
+        FilePath     = "$Env:SystemRoot\System32\msiexec.exe"
+        ArgumentList = "/package `"$($OutFile.FullName)`" /quiet /norestart DONOTCREATEDESKTOPSHORTCUT=true /log $LogFile"
+        NoNewWindow  = $true
+        Wait         = $true
+        PassThru     = $true
+        ErrorAction  = "Continue"
+    }
+    $result = Start-Process @params
+    Write-Information -MessageData ":: Install exit code: $($result.ExitCode)" -InformationAction "Continue"
+}
+catch {
+    throw $_
+}
+#endregion
