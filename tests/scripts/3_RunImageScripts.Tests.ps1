e0ce032c5d33d76e1cb3b7b9e248326d1c27c973
Aaron Parker <aaron@stealthpuppy.com>
2024-05-26 22:15:58 +1000

    Update 3_RunImageScripts.Tests.ps1

--- tests/scripts/3_RunImageScripts.Tests.ps1
+++ tests/scripts/3_RunImageScripts.Tests.ps1
@@ -24,13 +24,15 @@ BeforeDiscovery {
     $Path = "$Path\scripts\image"
     $SupportScripts = Get-ChildItem -Path $Path -Include "0*.ps1" -Recurse -Exclude "012_WindowsUpdate.ps1"
 
+    # These apps don't test well in pipelines / workers
     $ExcludeApps = "101_Avd-AgentMicrosoftWvdMultimediaRedirection.ps1", "101_Avd-AgentMicrosoftWvdRtcService.ps1"
     $DependencyScripts = Get-ChildItem -Path $Path -Include "1*.ps1" -Recurse -Exclude $ExcludeApps
 
     $MicrosoftAppsScripts = Get-ChildItem -Path $Path -Include "2*.ps1" -Recurse
 
+    # These apps don't test well in pipelines / workers
     $ExcludeApps = "420_1Password.ps1", "421_1PasswordCli.ps1", "412_MozillaFirefox.ps1", "417_CitrixWorkspaceApp.ps1"
-    $3rdPartyScripts = Get-ChildItem -Path $Path -Include "4*.ps1" -Recurse #-Exclude $ExcludeApps
+    $3rdPartyScripts = Get-ChildItem -Path $Path -Include "4*.ps1" -Recurse -Exclude $ExcludeApps
     $CleanupScripts = Get-ChildItem -Path $Path -Include "9*.ps1" -Recurse
 
     # Get scripts to run a 2nd time
