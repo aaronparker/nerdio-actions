bd5b772f8e9a7f0bc0676f230ec3e09cbb1d212c
Aaron Parker <aaron@stealthpuppy.com>
2024-05-27 10:24:53 +1000

    Update PowerToys

--- scripts/image/210_MicrosoftPowerToys.ps1
+++ scripts/image/210_MicrosoftPowerToys.ps1
@@ -39,22 +39,27 @@ $params = @{
 Start-Process @params
 
 # Disable features that aren't suitable for VDI
+reg add "HKLM\Software\Policies\PowerToys" /v "AllowExperimentation" /d 0 /t "REG_DWORD" /f | Out-Null
 reg add "HKLM\Software\Policies\PowerToys" /v "ConfigureEnabledUtilityAwake" /d 0 /t "REG_DWORD" /f | Out-Null
+reg add "HKLM\Software\Policies\PowerToys" /v "ConfigureEnabledUtilityEnvironmentVariables" /d 0 /t "REG_DWORD" /f | Out-Null
+reg add "HKLM\Software\Policies\PowerToys" /v "ConfigureEnabledUtilityFileLocksmith" /d 0 /t "REG_DWORD" /f | Out-Null
+reg add "HKLM\Software\Policies\PowerToys" /v "ConfigureEnabledUtilityFileExplorerGcodePreview" /d 0 /t "REG_DWORD" /f | Out-Null
+reg add "HKLM\Software\Policies\PowerToys" /v "ConfigureEnabledUtilityFileExplorerGcodeThumbnails" /d 0 /t "REG_DWORD" /f | Out-Null
 reg add "HKLM\Software\Policies\PowerToys" /v "ConfigureEnabledUtilityHostsFileEditor" /d 0 /t "REG_DWORD" /f | Out-Null
+reg add "HKLM\Software\Policies\PowerToys" /v "ConfigureEnabledUtilityFileExplorerPDFPreview" /d 0 /t "REG_DWORD" /f | Out-Null
 reg add "HKLM\Software\Policies\PowerToys" /v "ConfigureEnabledUtilityFileExplorerPDFThumbnails" /d 0 /t "REG_DWORD" /f | Out-Null
+reg add "HKLM\Software\Policies\PowerToys" /v "ConfigureEnabledUtilityFileExplorerQOIPreview" /d 0 /t "REG_DWORD" /f | Out-Null
+reg add "HKLM\Software\Policies\PowerToys" /v "ConfigureEnabledUtilityFileExplorerQOIThumbnails" /d 0 /t "REG_DWORD" /f | Out-Null
 reg add "HKLM\Software\Policies\PowerToys" /v "ConfigureEnabledUtilityFileExplorerSTLThumbnails" /d 0 /t "REG_DWORD" /f | Out-Null
 reg add "HKLM\Software\Policies\PowerToys" /v "ConfigureEnabledUtilityFileExplorerSVGThumbnails" /d 0 /t "REG_DWORD" /f | Out-Null
-reg add "HKLM\Software\Policies\PowerToys" /v "ConfigureEnabledUtilityFileExplorerGcodeThumbnails" /d 0 /t "REG_DWORD" /f | Out-Null
-reg add "HKLM\Software\Policies\PowerToys" /v "ConfigureEnabledUtilityFileExplorerPDFPreview" /d 0 /t "REG_DWORD" /f | Out-Null
-reg add "HKLM\Software\Policies\PowerToys" /v "ConfigureEnabledUtilityFileLocksmith" /d 0 /t "REG_DWORD" /f | Out-Null
 reg add "HKLM\Software\Policies\PowerToys" /v "ConfigureEnabledUtilityVideoConferenceMute" /d 0 /t "REG_DWORD" /f | Out-Null
-reg add "HKLM\Software\Policies\PowerToys" /v "ConfigureEnabledUtilityPeek" /d 0 /t "REG_DWORD" /f | Out-Null
-reg add "HKLM\Software\Policies\PowerToys" /v "AllowExperimentation" /d 0 /t "REG_DWORD" /f | Out-Null
-reg add "HKLM\Software\Policies\PowerToys" /v "ConfigureEnabledUtilityEnvironmentVariables" /d 0 /t "REG_DWORD" /f | Out-Null
-reg add "HKLM\Software\Policies\PowerToys" /v "ConfigureEnabledUtilityMouseWithoutBorders" /d 0 /t "REG_DWORD" /f | Out-Null
-reg add "HKLM\Software\Policies\PowerToys" /v "PerUserInstallationDisabled" /d 1 /t "REG_DWORD" /f | Out-Null
-reg add "HKLM\Software\Policies\PowerToys" /v "SuspendNewUpdateAvailableToast" /d 1 /t "REG_DWORD" /f | Out-Null
+reg add "HKLM\Software\Policies\PowerToys" /v "DisableNewUpdateAvailableToast" /d 1 /t "REG_DWORD" /f | Out-Null
 reg add "HKLM\Software\Policies\PowerToys" /v "AutomaticUpdateDownloadDisabled" /d 1 /t "REG_DWORD" /f | Out-Null
+reg add "HKLM\Software\Policies\PowerToys" /v "PerUserInstallationDisabled" /d 1 /t "REG_DWORD" /f | Out-Null
+reg add "HKLM\Software\Policies\PowerToys" /v "DoNotShowWhatsNewAfterUpdates" /d 1 /t "REG_DWORD" /f | Out-Null
+reg add "HKLM\Software\Policies\PowerToys" /v "SuspendNewUpdateAvailableToast" /d 1 /t "REG_DWORD" /f | Out-Null
+reg add "HKLM\Software\Policies\PowerToys" /v "ConfigureEnabledUtilityCmdNotFound" /d 0 /t "REG_DWORD" /f | Out-Null
+reg add "HKLM\Software\Policies\PowerToys" /v "ConfigureEnabledUtilityPeek" /d 0 /t "REG_DWORD" /f | Out-Null
 
 Start-Sleep -Seconds 5
 Get-Process -ErrorAction "SilentlyContinue" | `
