4f8e0f2a56a1cdf1764ade3617b248b655603601
Aaron Parker <aaron@stealthpuppy.com>
2025-07-27 14:57:56 +1000

    Update New-ShellApp.ps1
    Update with parameters and additional error checking

--- shell-apps/New-ShellApp.ps1
+++ shell-apps/New-ShellApp.ps1
@@ -1,36 +1,44 @@
+#Requires -Module Az.Accounts, Az.Storage, Evergreen
 <#
     Automate the import of a Nerdio Manager Shell App
 #>
+[CmdletBinding()]
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute("PSAvoidUsingPlainTextForPassword", "", Justification = "Credential are protected locally.")]
+param (
+    [Parameter(Mandatory = $false)]
+    [System.String] $EnvironmentFile = "/Users/aaron/projects/nerdio-actions/api/environment.json",
 
-# Environment variables
-$Path = "/Users/aaron/projects/nerdio-actions"
-$env = Get-Content -Path "$Path/shell-apps/environment.json" | ConvertFrom-Json
-$creds = Get-Content -Path "$Path/api/creds.json" | ConvertFrom-Json
+    [Parameter(Mandatory = $false)]
+    [System.String] $CredentialsFile = "/Users/aaron/projects/nerdio-actions/api/creds.json",
+
+    [Parameter(Mandatory = $false)]
+    [System.String] $TempPath = "/Users/aaron/Temp/shell-apps",
+    
+    [Parameter(Mandatory = $false)]
+    [System.String] $AppPath = "/Users/aaron/projects/nerdio-actions/shell-apps/Microsoft/VisualStudioCode"
+)
+
+# Read environment variables and credentials
+$ErrorActionPreference = "Stop"
+$env = Get-Content -Path $EnvironmentFile | ConvertFrom-Json
+$creds = Get-Content -Path $CredentialsFile | ConvertFrom-Json
 
 # Create new Shell App definition
-$Definition = Get-Content -Path "$Path/shell-apps/Microsoft/VisualStudioCode/Definition.json" | ConvertFrom-Json
-$InstallScript = Get-Content -Path "$Path/shell-apps/Microsoft/VisualStudioCode/Install.ps1" -Raw
-$UninstallScript = Get-Content -Path "$Path/shell-apps/Microsoft/VisualStudioCode/Uninstall.ps1" -Raw
-$DetectScript = Get-Content -Path "$Path/shell-apps/Microsoft/VisualStudioCode/Detect.ps1" -Raw
+$Definition = Get-Content -Path "$AppPath/Definition.json" | ConvertFrom-Json
+$InstallScript = Get-Content -Path "$AppPath/Install.ps1" -Raw
+$UninstallScript = Get-Content -Path "$AppPath/Uninstall.ps1" -Raw
+$DetectScript = Get-Content -Path "$AppPath/Detect.ps1" -Raw
 
 # Get the application details via Evergreen and download
-New-Item -Path "/Users/aaron/Temp/shell-apps" -ItemType "Directory" -Force | Out-Null
 Write-Host -ForegroundColor "Cyan" "Query Evergreen: $($Definition.source.app)"
 Write-Host -ForegroundColor "Cyan" "Filter: $($Definition.source.filter)"
 $App = Get-EvergreenApp -Name $Definition.source.app | Where-Object { Invoke-Expression "$($Definition.source.filter)" }
 Write-Host -ForegroundColor "Cyan" "Found version $($App.Version)"
-$File = $App | Save-EvergreenApp -LiteralPath "/Users/aaron/Temp/shell-apps"
+New-Item -Path $TempPath -ItemType "Directory" -Force | Out-Null
+$File = $App | Save-EvergreenApp -LiteralPath $TempPath
 Write-Host -ForegroundColor "Cyan" "Downloaded file: $($File.FullName)"
 
-# Get the SHA256 hash of the file
-if ($null -eq $App.Sha256) {
-    $Sha256 = Get-FileHash -Path $File.FullName -Algorithm "SHA256"
-}
-else {
-    $Sha256 = $App.Sha256
-}
-
-# Authenticate to Azure (manual authentication)
+# Authenticate to Azure (manual authentication - update in a pipeline to use a managed identity)
 if ($null -eq (Get-AzContext | Where-Object { $_.Subscription.Id -eq $creds.SubscriptionId })) {
     Write-Host -ForegroundColor "Cyan" "Authenticate to Azure"
     Connect-AzAccount -UseDeviceAuthentication -TenantId $creds.tenantId -Subscription $creds.subscriptionId
@@ -44,15 +52,24 @@ $Context = New-AzStorageContext -StorageAccountName $env.storageAccountName -Sto
 # Upload file to blob container
 $BlobName = "$(New-Guid)-$(Split-Path -Path $File.FullName -Leaf)"
 $BlobFile = Set-AzStorageBlobContent -File $File.FullName -Container $env.containerName -Blob $BlobName -Context $Context
-Write-Host -ForegroundColor "Cyan" "Uploaded file to blob: $($BlobFile.ICloudBlob.Uri.AbsoluteUri)"
+if ($null -eq $BlobFile.ICloudBlob.Uri.AbsoluteUri) {
+    Write-Error -Message "Failed to upload blob file to storage account."
+    exit 1
+}
+else {
+    Write-Host -ForegroundColor "Cyan" "Uploaded file to blob: $($BlobFile.ICloudBlob.Uri.AbsoluteUri)"
+}
 
 # Update the app definition
 $Definition.detectScript = $DetectScript
 $Definition.installScript = $InstallScript
 $Definition.uninstallScript = $UninstallScript
+if ($File.FullName -match "\.zip$") {
+    $Definition.fileUnzip = $true
+}
 $Definition.versions[0].name = $App.Version
 $Definition.versions[0].file.sourceUrl = $BlobFile.ICloudBlob.Uri.AbsoluteUri
-$Definition.versions[0].file.sha256 = $Sha256
+$Definition.versions[0].file.sha256 = $(if ($null -eq $App.Sha256) { (Get-FileHash -Path $File.FullName -Algorithm "SHA256").Hash } else { $App.Sha256 })
 $DefinitionJson = $Definition | Select-Object -ExcludeProperty "source" | ConvertTo-Json -Depth 10
 
 # Authenticate to Nerdio Manager
@@ -77,22 +94,24 @@ $Token = Invoke-RestMethod @params
 Write-Host -ForegroundColor "Cyan" "Authenticated to Nerdio Manager."
 
 # Create the Shell App in Nerdio Manager
-$params = @{
-    Uri             = "https://$($env.nmeHost)/api/v1/shell-app"
-    Method          = "POST"
-    Headers         = @{
-        "accept"        = "application/json"
-        "Authorization" = "Bearer $($Token.access_token)"
+if ($Token) {
+    $params = @{
+        Uri             = "https://$($env.nmeHost)/api/v1/shell-app"
+        Method          = "POST"
+        Headers         = @{
+            "accept"        = "application/json"
+            "Authorization" = "Bearer $($Token.access_token)"
+        }
+        Body            = $DefinitionJson
+        ContentType     = "application/json"
+        UseBasicParsing = $true
+        ErrorAction     = "Stop"
     }
-    Body            = $DefinitionJson
-    ContentType     = "application/json"
-    UseBasicParsing = $true
-    ErrorAction     = "Stop"
-}
-$Result = Invoke-RestMethod @params
-if ($Result.job.status -eq "Completed") {
-    Write-Host -ForegroundColor "Green" "Shell App created successfully. Id: $($Result.job.id)"
-}
-else {
-    Write-Host -ForegroundColor "Red" "Failed to create Shell App. Status: $($Result.job.status)"
+    $Result = Invoke-RestMethod @params
+    if ($Result.job.status -eq "Completed") {
+        Write-Host -ForegroundColor "Green" "Shell App created successfully. Id: $($Result.job.id)"
+    }
+    else {
+        Write-Host -ForegroundColor "Red" "Failed to create Shell App. Status: $($Result.job.status)"
+    }
 }
