6dcd5488c6f54230caa205e62e73dc5bad06d615
Aaron Parker <aaron@stealthpuppy.com>
2025-07-28 10:14:27 +1000

    Update New-ShellApp.ps1
    Add UseRemoteUrl parameter

--- shell-apps/New-ShellApp.ps1
+++ shell-apps/New-ShellApp.ps1
@@ -15,10 +15,40 @@ param (
     [System.String] $CredentialsFile = "/Users/aaron/projects/nerdio-actions/api/creds.json",
 
     [Parameter(Mandatory = $false)]
-    [System.String] $TempPath = "/Users/aaron/Temp/shell-apps"
+    [System.String] $TempPath = "/Users/aaron/Temp/shell-apps",
+
+    [Parameter(Mandatory = $false)]
+    [System.Management.Automation.SwitchParameter] $UseRemoteUrl
 )
 
 begin {
+    #functions
+    function Get-RemoteFileHash {
+        param (
+            [Parameter(Mandatory = $true)]
+            [System.String] $Url
+        )
+        try {
+            $WebClient = [System.Net.WebClient]::new()
+            Write-Host -ForegroundColor "Cyan" "Opening remote stream: $Url"
+            $Stream = $WebClient.OpenRead($Url)
+            if ($null -eq $Stream) {
+                throw "Failed to open remote stream. Stream is null."
+            }
+            Write-Host -ForegroundColor "Cyan" "Calculating SHA256 hash."
+            $hash = Get-FileHash -Algorithm "SHA256" -InputStream $Stream
+            Write-Host -ForegroundColor "Cyan" "Hash: $($hash.Hash)"
+            return $hash.Hash
+        }
+        catch {
+            Write-Error "Error occurred: $($_.Exception.Message)"
+        }
+        finally {
+            if ($Stream) { $Stream.Dispose() }
+            if ($WebClient) { $WebClient.Dispose() }
+        }
+    }
+
     # Read environment variables and credentials
     $ErrorActionPreference = "Stop"
     $env = Get-Content -Path $EnvironmentFile | ConvertFrom-Json
@@ -67,48 +97,75 @@ process {
             $UninstallScript = Get-Content -Path "$Path/Uninstall.ps1" -Raw
             $DetectScript = Get-Content -Path "$Path/Detect.ps1" -Raw
 
-            # Get the application details via Evergreen and download
+            # Get the application details via Evergreen
             Write-Host -ForegroundColor "Cyan" "Query Evergreen: $($Definition.source.app)"
             Write-Host -ForegroundColor "Cyan" "Filter: $($Definition.source.filter)"
             $App = Get-EvergreenApp -Name $Definition.source.app | Where-Object { Invoke-Expression "$($Definition.source.filter)" }
             Write-Host -ForegroundColor "Cyan" "Found version $($App.Version)"
-            New-Item -Path $TempPath -ItemType "Directory" -Force | Out-Null
-            $File = $App | Save-EvergreenApp -LiteralPath $TempPath
-            Write-Host -ForegroundColor "Cyan" "Downloaded file: $($File.FullName)"
 
-            # Get storage account key; Create storage context
-            Write-Host -ForegroundColor "Cyan" "Get storage acccount key from: $($env.resourceGroupName) / $($env.storageAccountName)"
-            $StorageAccountKey = (Get-AzStorageAccountKey -ResourceGroupName $env.resourceGroupName -Name $env.storageAccountName)[0].Value
-            $Context = New-AzStorageContext -StorageAccountName $env.storageAccountName -StorageAccountKey $StorageAccountKey
+            if ($UseRemoteUrl) {
+                # Use the remote URL to get the file
+                if ($null -eq $App.Sha256) {
+                    $Sha256 = Get-RemoteFileHash -Url $App.URI
+                }
+                else {
+                    $Sha256 = $App.Sha256
+                }
 
-            # Upload file to blob container
-            # Permissions required: "Storage Blob Data Contributor"
-            $BlobName = "$(New-Guid)-$(Split-Path -Path $File.FullName -Leaf)"
-            $params = @{
-                File      = $File.FullName
-                Container = $env.containerName
-                Blob      = $BlobName
-                Context   = $Context
-            }
-            $BlobFile = Set-AzStorageBlobContent @params
-            if ($null -eq $BlobFile.ICloudBlob.Uri.AbsoluteUri) {
-                Write-Error -Message "Failed to upload blob file to storage account."
-                exit 1
+                # Set the source URL to the remote URI
+                $SourceUrl = $App.URI
             }
             else {
-                Write-Host -ForegroundColor "Cyan" "Uploaded file to blob: $($BlobFile.ICloudBlob.Uri.AbsoluteUri)"
-            }
+                # Download the application binary
+                New-Item -Path $TempPath -ItemType "Directory" -Force | Out-Null
+                $File = $App | Save-EvergreenApp -LiteralPath $TempPath
+                Write-Host -ForegroundColor "Cyan" "Downloaded file: $($File.FullName)"
 
-            # Get a SAS token for the blob
-            $params = @{
-                Context    = $Context
-                Container  = $env.containerName
-                Blob       = $BlobName
-                Permission = "r"
-                ExpiryTime = (Get-Date).AddYears(1)
-                FullUri    = $true
+                # Determine the SHA256 hash of the file
+                if ($null -eq $App.Sha256) {
+                    $Sha256 = (Get-FileHash -Path $File.FullName -Algorithm "SHA256").Hash
+                }
+                else {
+                    $Sha256 = $App.Sha256
+                }
+
+                # Get storage account key; Create storage context
+                Write-Host -ForegroundColor "Cyan" "Get storage acccount key from: $($env.resourceGroupName) / $($env.storageAccountName)"
+                $StorageAccountKey = (Get-AzStorageAccountKey -ResourceGroupName $env.resourceGroupName -Name $env.storageAccountName)[0].Value
+                $Context = New-AzStorageContext -StorageAccountName $env.storageAccountName -StorageAccountKey $StorageAccountKey
+
+                # Upload file to blob container
+                # Permissions required: "Storage Blob Data Contributor"
+                $BlobName = "$((New-Guid) -replace '-', '').$(Split-Path -Path $File.FullName -Leaf)"
+                $params = @{
+                    File      = $File.FullName
+                    Container = $env.containerName
+                    Blob      = $BlobName
+                    Context   = $Context
+                }
+                $BlobFile = Set-AzStorageBlobContent @params
+                if ($null -eq $BlobFile.ICloudBlob.Uri.AbsoluteUri) {
+                    Write-Error -Message "Failed to upload blob file to storage account."
+                    exit 1
+                }
+                else {
+                    Write-Host -ForegroundColor "Cyan" "Uploaded file to blob: $($BlobFile.ICloudBlob.Uri.AbsoluteUri)"
+                }
+
+                # Get a SAS token for the blob
+                $params = @{
+                    Context    = $Context
+                    Container  = $env.containerName
+                    Blob       = $BlobName
+                    Permission = "r"
+                    ExpiryTime = (Get-Date).AddYears(10)
+                    FullUri    = $true
+                }
+                $SasToken = New-AzStorageBlobSASToken @params
+
+                # Determine the source URL, if a SAS token is provided, use it; otherwise, use the blob URI
+                $(if ($SasToken) { $SourceUrl = $SasToken } else { $SourceUrl = $BlobFile.ICloudBlob.Uri.AbsoluteUri })
             }
-            $SasToken = New-AzStorageBlobSASToken @params
 
             # Update the app definition
             $Definition.detectScript = $DetectScript
@@ -118,8 +175,8 @@ process {
                 $Definition.fileUnzip = $true
             }
             $Definition.versions[0].name = $App.Version
-            $Definition.versions[0].file.sourceUrl = $(if ($SasToken) { $SasToken } else { $BlobFile.ICloudBlob.Uri.AbsoluteUri })
-            $Definition.versions[0].file.sha256 = $(if ($null -eq $App.Sha256) { (Get-FileHash -Path $File.FullName -Algorithm "SHA256").Hash } else { $App.Sha256 })
+            $Definition.versions[0].file.sourceUrl = $SourceUrl
+            $Definition.versions[0].file.sha256 = $Sha256
             $DefinitionJson = $Definition | Select-Object -ExcludeProperty "source" | ConvertTo-Json -Depth 10
 
             # Create the Shell App in Nerdio Manager
