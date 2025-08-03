5829da8c42c4e5608cc28c4f73c4735b16eb69bf
Aaron Parker <aaron@stealthpuppy.com>
2025-08-03 20:57:40 +1000

    Update Create-ShellApps.ps1
    Update for running on Mac and Windows, updates to prune versions from a Shell App with more than 2 versions

--- shell-apps/Create-ShellApps.ps1
+++ shell-apps/Create-ShellApps.ps1
@@ -1,8 +1,17 @@
-Import-Module -Name "./NerdioShellApps.psm1" -Force
+# Import the required module; Read environment variables and credentials
+if ($IsWindows) {
+    Import-Module -Name "Z:\projects\nerdio-actions\shell-apps\NerdioShellApps.psm1" -Force
+    $EnvironmentFile = "Z:\projects\nerdio-actions\api\environment.json"
+    $CredentialsFile = "Z:\projects\nerdio-actions\api\creds.json"
+    $Path = "Z:\projects\nerdio-actions\shell-apps"
+}
+else {
+    Import-Module -Name "/Users/aaron/projects/nerdio-actions/shell-apps/NerdioShellApps.psm1" -Force
+    $EnvironmentFile = "/Users/aaron/projects/nerdio-actions/api/environment.json"
+    $CredentialsFile = "/Users/aaron/projects/nerdio-actions/api/creds.json"
+    $Path = "/Users/aaron/projects/nerdio-actions/shell-apps"
+}
 
-# Read environment variables and credentials
-$EnvironmentFile = "/Users/aaron/projects/nerdio-actions/api/environment.json"
-$CredentialsFile = "/Users/aaron/projects/nerdio-actions/api/creds.json"
 $Env = Get-Content -Path $EnvironmentFile | ConvertFrom-Json
 $Creds = Get-Content -Path $CredentialsFile | ConvertFrom-Json
 $params = @{
@@ -21,15 +30,12 @@ Set-NmeCredentials @params
 Connect-Nme
 
 # Authenticate to Azure (manual authentication - update in a pipeline to use a managed identity)
-$Creds = Get-Content -Path "/Users/aaron/projects/nerdio-actions/api/creds.json" | ConvertFrom-Json
 if ($null -eq (Get-AzContext | Where-Object { $_.Subscription.Id -eq $Creds.SubscriptionId })) {
     Write-Information -MessageData "$($PSStyle.Foreground.Cyan)Authenticate to Azure"
     Connect-AzAccount -UseDeviceAuthentication -TenantId $Creds.tenantId -Subscription $Creds.subscriptionId
 }
 
-$Path = "/Users/aaron/projects/nerdio-actions/shell-apps"
 $Paths = Get-ChildItem -Path $Path -Include "Definition.json" -Recurse | ForEach-Object { $_ | Select-Object -ExpandProperty "DirectoryName" }
-
 foreach ($Path in $Paths) {
     $Def = Get-ShellAppDefinition -Path $Path
     $App = Get-EvergreenAppDetail -Definition $Def
@@ -55,16 +61,40 @@ foreach ($Path in $Paths) {
 
 # Export the list of Shell Apps, showing the latest version for each app
 (Get-ShellApp).items | ForEach-Object {
+    $ExistingVersions = (Get-ShellAppVersion -Id $_.id).items | `
+        Where-Object { $_.isPreview -eq $false } | `
+        Sort-Object -Property @{ Expression = { [System.Version]$_.Version }; Descending = $true }
     [PSCustomObject]@{
         publisher     = $_.publisher
         name          = $_.name
-        latestVersion = ((Get-ShellAppVersion -Id $_.id).items | `
-                Where-Object { $_.isPreview -eq $false } | `
-                Sort-Object -Property @{ Expression = { [System.Version]$_.Version }; Descending = $true } | `
-                Select-Object -First 1).name
+        versionCount  = $ExistingVersions | Measure-Object | Select-Object -ExpandProperty "Count"
+        latestVersion = ($ExistingVersions | Select-Object -First 1).name
         createdAt     = $_.createdAt
         fileUnzip      = $_.fileUnzip
         isPublic      = $_.isPublic
         id            = $_.id
     }
 } | Format-Table -AutoSize
+
+# Remove the latest version of each Shell App if there are more than 2 versions
+$KeepCount = 2
+(Get-ShellApp).items | ForEach-Object {
+    $ExistingVersions = (Get-ShellAppVersion -Id $_.id).items | `
+        Where-Object { $_.isPreview -eq $false } | `
+        Sort-Object -Property @{ Expression = { [System.Version]$_.Version }; Descending = $true }
+    if ($ExistingVersions.Count -gt $KeepCount) {
+        $VersionsToRemove = $ExistingVersions | Select-Object -Skip ($ExistingVersions.Count - $KeepCount)
+        foreach ($Version in $VersionsToRemove) {
+            Write-Information -MessageData "$($PSStyle.Foreground.Cyan)Removing Shell App Version: $($_.name) $($Version.name)"
+            $Result = Remove-ShellAppVersion -Id $_.id -Version $Version.name
+            if ($Result.job.status -eq "Completed") {
+                $File = $Version.file.sourceUrl -split "\?"
+                $FileName = $File -split "/" | Select-Object -Last 1
+                Remove-AzStorageBlob -Container $Env.containerName -Blob $FileName
+            }
+        }
+    }
+    else {
+        Write-Information -MessageData "$($PSStyle.Foreground.Yellow)No versions to remove for Shell App: $($_.name)"
+    }
+}
