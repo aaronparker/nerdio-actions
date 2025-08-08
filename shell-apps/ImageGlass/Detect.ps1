474d62f0b2bc1d3e0ee16eba334364811cec484e
Aaron Parker <aaron@stealthpuppy.com>
2025-08-08 21:45:30 +1000

    Update Detect.ps1
    Use FileVersion instead of ProductVersion

--- shell-apps/ImageGlass/Detect.ps1
+++ shell-apps/ImageGlass/Detect.ps1
@@ -6,12 +6,12 @@ if (Test-Path -Path $FilePath) {
     $FileItem = Get-ChildItem -Path $FilePath -ErrorAction "SilentlyContinue"
     $Context.Log("File found: $($FileItem.FullName)")
     $FileInfo = [Diagnostics.FileVersionInfo]::GetVersionInfo($FileItem.FullName)
-    if ([System.Version]::Parse($FileInfo.ProductVersion) -ge [System.Version]::Parse($Context.TargetVersion)) {
-        $Context.Log("No update required. Found '$($FileInfo.ProductVersion)' against '$($Context.TargetVersion)'.")
+    if ([System.Version]::Parse($FileInfo.FileVersion) -ge [System.Version]::Parse($Context.TargetVersion)) {
+        $Context.Log("No update required. Found '$($FileInfo.FileVersion)' against '$($Context.TargetVersion)'.")
         return $true
     }
     else {
-        $Context.Log("Update required. Found '$($FileInfo.ProductVersion)' less than '$($Context.TargetVersion)'.")
+        $Context.Log("Update required. Found '$($FileInfo.FileVersion)' less than '$($Context.TargetVersion)'.")
         if ($Context.Versions -is [System.Array]) { return $null } else { return $false }
     }
 }
