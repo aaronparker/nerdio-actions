ec53485460ab20c56c173b06bf1a5e1466bf55fc
Aaron Parker <aaron@stealthpuppy.com>
2025-08-12 19:15:28 +1000

    Update Detect.ps1

--- shell-apps/Obsidian/Detect.ps1
+++ shell-apps/Obsidian/Detect.ps1
@@ -2,15 +2,12 @@
 [System.String] $FilePath = "${Env:ProgramFiles}\Obsidian\Obsidian.exe"
 
 # Detection logic
+# Detection logic
 if (Test-Path -Path $FilePath) {
     $FileItem = Get-ChildItem -Path $FilePath -ErrorAction "SilentlyContinue"
     $Context.Log("File found: $($FileItem.FullName)")
     $FileInfo = [Diagnostics.FileVersionInfo]::GetVersionInfo($FileItem.FullName)
-    $FileVersion = [System.Version]::Parse(($FileInfo.ProductVersion -replace ",", "."))
-    $FileVersionJoin = $FileVersion.Major, $FileVersion.Minor, $FileVersion.Build -join "."
-    $Context.Log("Found version: $FileVersionJoin")
-    $Context.Log("Compare to: $($Context.TargetVersion)")
-    if ([System.Version]::Parse($FileVersionJoin) -ge [System.Version]::Parse($Context.TargetVersion)) {
+    if ([System.Version]::Parse($FileInfo.ProductVersion) -ge [System.Version]::Parse($Context.TargetVersion)) {
         $Context.Log("No update required. Found '$($FileInfo.ProductVersion)' against '$($Context.TargetVersion)'.")
         if ($Context.Versions -is [System.Array]) { return $FileInfo.ProductVersion } else { return $true }
     }
