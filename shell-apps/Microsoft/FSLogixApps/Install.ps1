97441f43cea5b8d0d4fda85265893bfe48515a44
Aaron Parker <aaron@stealthpuppy.com>
2025-08-14 16:54:57 +1000

    Add Update-ShellApp to pipeline

--- shell-apps/Microsoft/FSLogixApps/Install.ps1
+++ shell-apps/Microsoft/FSLogixApps/Install.ps1
@@ -2,7 +2,6 @@ $Context.Log("Installing FSLogix Apps")
 foreach ($File in "FSLogixAppsSetup.exe") {
     $Installers = Get-ChildItem -Path $PWD -Recurse -Include $File | Where-Object { $_.Directory -match "x64" }
     foreach ($Installer in $Installers) {
-        $LogFile = "$Env:Windows\Logs\$($Installer.Name)$($App.Version).log" -replace " ", ""
         $Context.Log("Installing Microsoft FSLogix Apps agent from: $($Installer.FullName)")
         $params = @{
             FilePath     = $Installer.FullName
