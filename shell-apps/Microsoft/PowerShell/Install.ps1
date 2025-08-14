577dbc71b72e788b997c59e0f0e96f46f2bcc877
Aaron Parker <aaron@stealthpuppy.com>
2025-08-14 14:23:58 +1000

    Update Install.ps1
    Update command line

--- shell-apps/Microsoft/PowerShell/Install.ps1
+++ shell-apps/Microsoft/PowerShell/Install.ps1
@@ -1,7 +1,7 @@
 $Context.Log("Installing Microsoft PowerShell")
 $params = @{
-    FilePath     = $Context.GetAttachedBinary()
-    ArgumentList = "/install /quiet /norestart"
+    FilePath     = "$Env:SystemRoot\System32\msiexec.exe"
+    ArgumentList = "/package $($Context.GetAttachedBinary()) /quiet /norestart"
     Wait         = $true
     NoNewWindow  = $true
     ErrorAction  = "Stop"
