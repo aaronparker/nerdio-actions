87274ba23d0cbfdc72e1b8663003cc5e93b735dc
Aaron Parker <aaron@stealthpuppy.com>
2025-08-17 11:31:42 +1000

    Update Install.ps1
    Add delay to ensure shortcut is created before delete

--- shell-apps/Obsidian/Install.ps1
+++ shell-apps/Obsidian/Install.ps1
@@ -8,9 +8,9 @@ $params = @{
 }
 $result = Start-Process @params
 $Context.Log("Install complete. Return code: $($result.ExitCode)")
-$Context.Log("Install complete")
 
 # Delete public desktop shortcut
+Start-Sleep -Seconds 5
 $Shortcuts = @("$Env:Public\Desktop\Obsidian.lnk")
 Get-Item -Path $Shortcuts -ErrorAction "SilentlyContinue" | `
     ForEach-Object { $Context.Log("Remove file: $($_.FullName)"); Remove-Item -Path $_.FullName -Force -ErrorAction "SilentlyContinue" }
