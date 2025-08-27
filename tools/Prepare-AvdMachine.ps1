ffb8fda3442ca095fca727a630ad2cd8904f0a19
Aaron Parker <aaron@stealthpuppy.com>
2025-08-27 23:25:14 +1000

    Update script

--- tools/Prepare-AvdMachine.ps1
+++ tools/Prepare-AvdMachine.ps1
@@ -1,3 +1,6 @@
+<#
+    Remove or disable Windows 365 agents and components
+#>
 [CmdletBinding(SupportsShouldProcess = $false)]
 param (
     [Parameter(Mandatory = $false)]
@@ -36,13 +39,13 @@ function Get-InstalledSoftware {
     return $Apps
 }
 
+# Query API for Azure VM information
 $params = @{
     Headers = @{"Metadata" = "true" }
     Method  = "GET"
     Uri     = "http://169.254.169.254/metadata/instance?api-version=2021-02-01"
 }
 $result = Invoke-RestMethod @params
-
 if ($result.compute.tagsList.name -match "ms.inv*") {
     # Windows 365
     Write-Information -MessageData "Running in Windows 365"
@@ -62,6 +65,18 @@ else {
     reg export "HKLM\SOFTWARE\Microsoft\Windows365" $Path\Windows365.reg | Out-Null
     Write-Information -MessageData "Delete: HKLM\SOFTWARE\Microsoft\Windows365"
     reg delete "HKLM\SOFTWARE\Microsoft\Windows365" /f | Out-Null
+
+    Write-Information -MessageData "Unregistering Scheduled Task: SetDeviceModel"
+    Get-ScheduledTask -TaskName "SetDeviceModel" | Unregister-ScheduledTask
+    $params = @{
+        Path         = "HKLM:\SYSTEM\ControlSet001\Control\SystemInformation"
+        Name         = "SystemProductName"
+        Value        = "Virtual Machine"
+        PropertyType = "String"
+        Force        = $true
+    }
+    New-ItemProperty @params
+
     Get-Service -Name "CloudManagedDesktopExtension" -ErrorAction "SilentlyContinue" | ForEach-Object {
         Write-Information -MessageData "Stopping: $($_.Name)"
         Stop-Service -Name $_.Name
