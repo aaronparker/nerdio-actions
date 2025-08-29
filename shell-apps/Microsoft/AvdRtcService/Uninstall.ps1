3878d5d61012cc4a8ecd6bbca2a3b3d92d58b513
Aaron Parker <aaron@stealthpuppy.com>
2025-08-30 09:01:57 +1000

    Simplify function: Get-InstalledSoftware

--- shell-apps/Microsoft/AvdRtcService/Uninstall.ps1
+++ shell-apps/Microsoft/AvdRtcService/Uninstall.ps1
@@ -1,26 +1,17 @@
 function Get-InstalledSoftware {
-    [CmdletBinding()]
-    param ()
-    $UninstallKeys = @(
-        "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\*",
-        "HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*"
-    )
-
-    $Apps = @()
-    foreach ($Key in $UninstallKeys) {
-        try {
-            $propertyNames = "DisplayName", "DisplayVersion", "Publisher", "UninstallString", "PSPath", "WindowsInstaller", "InstallDate", "InstallSource", "HelpLink", "Language", "EstimatedSize", "SystemComponent"
-            $Apps += Get-ItemProperty -Path $Key -Name $propertyNames -ErrorAction "SilentlyContinue" | `
-                . { process { if ($null -ne $_.DisplayName) { $_ } } } | `
-                Where-Object { $_.SystemComponent -ne 1 } | `
-                Select-Object -Property @{n = "Name"; e = { $_.DisplayName } }, @{n = "Version"; e = { $_.DisplayVersion } }, "Publisher", "UninstallString", @{n = "RegistryPath"; e = { $_.PSPath -replace "Microsoft.PowerShell.Core\\Registry::", "" } }, "PSChildName", "WindowsInstaller", "InstallDate", "InstallSource", "HelpLink", "Language", "EstimatedSize" | `
-                Sort-Object -Property "Name", "Publisher"
-        }
-        catch {
-            throw $_.Exception.Message
-        }
+    $PropertyNames = "DisplayName", "DisplayVersion", "Publisher", "UninstallString", "PSPath", "WindowsInstaller",
+    "InstallDate", "InstallSource", "HelpLink", "Language", "EstimatedSize", "SystemComponent"
+    ("HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\*",
+    "HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*") | `
+        ForEach-Object {
+        Get-ItemProperty -Path $_ -Name $PropertyNames -ErrorAction "SilentlyContinue" | `
+            . { process { if ($null -ne $_.DisplayName) { $_ } } } | `
+            Where-Object { $_.SystemComponent -ne 1 } | `
+            Select-Object -Property @{n = "Name"; e = { $_.DisplayName } }, @{n = "Version"; e = { $_.DisplayVersion } }, "Publisher",
+        "UninstallString", @{n = "RegistryPath"; e = { $_.PSPath -replace "Microsoft.PowerShell.Core\\Registry::", "" } },
+        "PSChildName", "WindowsInstaller", "InstallDate", "InstallSource", "HelpLink", "Language", "EstimatedSize" | `
+            Sort-Object -Property "Name", "Publisher"
     }
-    return $Apps
 }
 
 Get-InstalledSoftware | Where-Object { $_.Name -match "Remote Desktop WebRTC Redirector Service*" } | ForEach-Object {
--- shell-apps/Microsoft/VisualC++2022x86/Detect.ps1
+++ shell-apps/Microsoft/VisualC++2022x86/Detect.ps1
@@ -3,28 +3,19 @@
 [System.String] $DisplayName = "Microsoft Visual C\+\+ 2015-2022 Redistributable \(x86\)*"
 
 function Get-InstalledSoftware {
-    [CmdletBinding()]
-    param ()
-    $UninstallKeys = @(
-        "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\*",
-        "HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*"
-    )
-
-    $Apps = @()
-    foreach ($Key in $UninstallKeys) {
-        try {
-            $propertyNames = "DisplayName", "DisplayVersion", "Publisher", "UninstallString", "PSPath", "WindowsInstaller", "InstallDate", "InstallSource", "HelpLink", "Language", "EstimatedSize", "SystemComponent"
-            $Apps += Get-ItemProperty -Path $Key -Name $propertyNames -ErrorAction "SilentlyContinue" | `
-                . { process { if ($null -ne $_.DisplayName) { $_ } } } | `
-                Where-Object { $_.SystemComponent -ne 1 } | `
-                Select-Object -Property @{n = "Name"; e = { $_.DisplayName } }, @{n = "Version"; e = { $_.DisplayVersion } }, "Publisher", "UninstallString", @{n = "RegistryPath"; e = { $_.PSPath -replace "Microsoft.PowerShell.Core\\Registry::", "" } }, "PSChildName", "WindowsInstaller", "InstallDate", "InstallSource", "HelpLink", "Language", "EstimatedSize" | `
-                Sort-Object -Property "Name", "Publisher"
-        }
-        catch {
-            throw $_.Exception.Message
-        }
+    $PropertyNames = "DisplayName", "DisplayVersion", "Publisher", "UninstallString", "PSPath", "WindowsInstaller",
+    "InstallDate", "InstallSource", "HelpLink", "Language", "EstimatedSize", "SystemComponent"
+    ("HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\*",
+    "HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*") | `
+        ForEach-Object {
+        Get-ItemProperty -Path $_ -Name $PropertyNames -ErrorAction "SilentlyContinue" | `
+            . { process { if ($null -ne $_.DisplayName) { $_ } } } | `
+            Where-Object { $_.SystemComponent -ne 1 } | `
+            Select-Object -Property @{n = "Name"; e = { $_.DisplayName } }, @{n = "Version"; e = { $_.DisplayVersion } }, "Publisher",
+        "UninstallString", @{n = "RegistryPath"; e = { $_.PSPath -replace "Microsoft.PowerShell.Core\\Registry::", "" } },
+        "PSChildName", "WindowsInstaller", "InstallDate", "InstallSource", "HelpLink", "Language", "EstimatedSize" | `
+            Sort-Object -Property "Name", "Publisher"
     }
-    return $Apps
 }
 
 # Get the installed appplication
