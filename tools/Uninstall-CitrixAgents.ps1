d24d34317db5ed135cedeffdb37afb59b7a41d41
Aaron Parker <aaron@stealthpuppy.com>
2025-09-05 16:29:44 +1000

    Update functions

--- tools/Uninstall-CitrixAgents.ps1
+++ tools/Uninstall-CitrixAgents.ps1
@@ -90,30 +90,43 @@ begin {
     }
 
     function Get-InstalledSoftware {
-        [CmdletBinding()]
-        param ()
-        $UninstallKeys = @(
-            "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\*",
-            "HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*"
-        )
+        $PropertyNames = "DisplayName", "DisplayVersion", "Publisher", "UninstallString", "PSPath", "WindowsInstaller",
+        "InstallDate", "InstallSource", "HelpLink", "Language", "EstimatedSize", "SystemComponent"
+        ("HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\*",
+        "HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*") | `
+            ForEach-Object {
+            Get-ItemProperty -Path $_ -Name $PropertyNames -ErrorAction "SilentlyContinue" | `
+                . { process { if ($null -ne $_.DisplayName) { $_ } } } | `
+                Where-Object { $_.SystemComponent -ne 1 } | `
+                Select-Object -Property @{n = "Name"; e = { $_.DisplayName } }, @{n = "Version"; e = { $_.DisplayVersion } }, "Publisher",
+            "UninstallString", @{n = "RegistryPath"; e = { $_.PSPath -replace "Microsoft.PowerShell.Core\\Registry::", "" } },
+            "PSChildName", "WindowsInstaller", "InstallDate", "InstallSource", "HelpLink", "Language", "EstimatedSize" | `
+                Sort-Object -Property "Name", "Publisher"
+        }
+    }
 
-        $Apps = @()
-        foreach ($Key in $UninstallKeys) {
-            try {
-                $propertyNames = "DisplayName", "DisplayVersion", "Publisher", "UninstallString", "PSPath", "WindowsInstaller", "InstallDate", "InstallSource", "HelpLink", "Language", "EstimatedSize", "SystemComponent"
-                $Apps += Get-ItemProperty -Path $Key -Name $propertyNames -ErrorAction "SilentlyContinue" | `
-                    . { process { if ($null -ne $_.DisplayName) { $_ } } } | `
-                    Where-Object { $_.SystemComponent -ne 1 } | `
-                    Select-Object -Property @{n = "Name"; e = { $_.DisplayName } }, @{n = "Version"; e = { $_.DisplayVersion } }, "Publisher", "UninstallString", @{n = "RegistryPath"; e = { $_.PSPath -replace "Microsoft.PowerShell.Core\\Registry::", "" } }, "PSChildName", "WindowsInstaller", "InstallDate", "InstallSource", "HelpLink", "Language", "EstimatedSize" | `
-                    Sort-Object -Property "Name", "Publisher"
+    function ConvertTo-UninstallCommand {
+        [CmdletBinding()]
+        param (
+            [Parameter(Mandatory = $true, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
+            [System.String]$UninstallString
+        )
+        begin {
+            $regex = '(?i)"?([A-Z]:\\[^"]*?\.exe|[A-Za-z0-9._-]+\.exe)"?\s*((?:\S+\s*)*)'
+        }
+        process {
+            if ($UninstallString -match $regex) {
+                $Exe = $Matches[1]
+                $Arguments = $Matches[2]
+                [PSCustomObject]@{
+                    FilePath     = if ($Exe -match "msiexec.exe") { "$Env:SystemRoot\System32\msiexec.exe" } else { $Exe }
+                    ArgumentList = ($Arguments -replace '\s+', ' ').Trim()
+                }
             }
-            catch {
-                throw $_.Exception.Message
+            else {
+                return $null
             }
         }
-
-        Remove-PSDrive -Name "HKU" -ErrorAction "SilentlyContinue" | Out-Null
-        return $Apps
     }
 
     function Uninstall-CitrixAgent {
