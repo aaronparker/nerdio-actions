e78a44e65081ce1411576885200ac144df2ba657
Aaron Parker <aaron@stealthpuppy.com>
2025-09-21 14:40:48 +1000

    Remove unzip

--- shell-apps/Microsoft/FSLogixApps/Install.ps1
+++ shell-apps/Microsoft/FSLogixApps/Install.ps1
@@ -1,3 +1,13 @@
+# Configure the environment
+$ErrorActionPreference = [System.Management.Automation.ActionPreference]::Stop
+$InformationPreference = [System.Management.Automation.ActionPreference]::Continue
+$ProgressPreference = [System.Management.Automation.ActionPreference]::SilentlyContinue
+[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
+
+# Unzip the attached binary to the current directory
+Expand-Archive -Path $Context.GetAttachedBinary() -DestinationPath $PWD -Force
+
+# Install FSLogix Apps agent
 foreach ($File in "FSLogixAppsSetup.exe") {
     $Installers = Get-ChildItem -Path $PWD -Recurse -Include $File | Where-Object { $_.Directory -match "x64" }
     foreach ($Installer in $Installers) {
