a4ea11735ab9715958470f35ca8d105f4040e2c2
Aaron Parker <aaron@stealthpuppy.com>
2025-11-06 12:46:05 +1100

    Delete kiosk configs

--- kiosk/aumid.ps1
+++
@@ -1,7 +0,0 @@
-
-Get-AppxPackage | Where-Object { $_.Name -like "*Edge*" } | ForEach-Object {
-    $Package = $_
-    foreach ($id in (Get-AppxPackageManifest $Package).Package.Applications.Application.Id) {
-        "<App AppUserModelId=`"$($Package.PackageFamilyName)!$($id)`" />"
-    }
-} | Set-Clipboard
